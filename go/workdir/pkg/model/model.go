a39461d87f4cd61ac362d0c948141f84a7f9441c
unknowntpo <e850506@gmail.com>
2022-06-17 09:14:28 +0800

    Implement Pwd()

--- go/workdir/pkg/model/model.go
+++ go/workdir/pkg/model/model.go
@@ -3,16 +3,25 @@ package model
 import (
 	"fmt"
 	"os"
+	"path/filepath"
+	"runtime"
 )
+
+func Pwd() string {
+	_, f, _, _ := runtime.Caller(1)
+	return filepath.Dir(f)
+}
 
 func Setup() error {
 	wd, err := os.Getwd()
 	if err != nil {
 		return fmt.Errorf("failed to open file: %v", err)
 	}
-	fmt.Println("wd in model.Setup: %v", wd)
+	fmt.Printf("wd in model.Setup: %v\n", wd)
 
-	f, err := os.Open("../../.env")
+	pwd := Pwd()
+
+	f, err := os.Open(filepath.Join(pwd, "../../.env"))
 	// This will fail if you call Setup() in api package
 	// because the in api pkg, env file is at ../.env
 	if err != nil {
