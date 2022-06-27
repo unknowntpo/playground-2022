e96a0db2273d1d5cc203dc9c0ac9b43cae7d58cd
unknowntpo <e850506@gmail.com>
2022-06-27 12:56:00 +0800

    experiment of ginkgo tree structure
    LALA
        - before each1
        - context outer
            - before each2
            - context inner
                - it should print something
        - inner describe
            - before each3
            - context inside inner describe
                - it should print something
    
    The tree will be flatten into
    LALA - context outer - context inner
    LALA - inner describe - context inside inner describe

--- go/ginkgo/cal_test.go
+++ go/ginkgo/cal_test.go
@@ -1,6 +1,7 @@
 package cal
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/onsi/ginkgo"
@@ -26,23 +27,37 @@ var _ = ginkgo.Describe("Calculator", func() {
 			gomega.Expect(cal.Add(x, y)).To(gomega.Equal(expectResult))
 		},
 		table.Entry("1+1 should be 2", 1, 1, 2),
-		table.Entry("2+1 should be 3", 1, 1, 3),
+		table.Entry("2+1 should be 3", 2, 1, 3),
 	)
 })
 
 var _ = ginkgo.Describe("LALA", func() {
-	var cal Calculator
-
 	ginkgo.BeforeEach(func() {
-		cal = NewCal()
+		fmt.Println("before each1")
+	})
+	ginkgo.Context("context outer", func() {
+		ginkgo.BeforeEach(func() {
+			fmt.Println("before each2")
+		})
+
+		ginkgo.Context("context inner", func() {
+			fmt.Println("lala")
+			ginkgo.It("should print something inside context inner", func() {
+				gomega.Expect(1 + 1).To(gomega.Equal(2))
+			})
+		})
 	})
 
-	table.DescribeTable(
-		"calculate integer",
-		func(x, y, expectResult int) {
-			gomega.Expect(cal.Add(x, y)).To(gomega.Equal(expectResult))
-		},
-		table.Entry("1+1 should be 2", 1, 1, 2),
-		table.Entry("2+1 should be 3", 2, 1, 3),
-	)
+	ginkgo.Describe("inner describe", func() {
+		ginkgo.BeforeEach(func() {
+			fmt.Println("before each3")
+		})
+
+		ginkgo.Context("context inside inner describe", func() {
+			fmt.Println("abc")
+			ginkgo.It("should pring something", func() {
+				gomega.Expect(1 + 1).To(gomega.Equal(2))
+			})
+		})
+	})
 })
