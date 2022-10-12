5ee772d67b1e51ec0561c6892ee6cc3ee5ad8aea
unknowntpo <e850506@gmail.com>
2022-10-12 08:09:08 +0800

    rename index.js -> random.js

---
+++ js/wds/promise/random.js
@@ -0,0 +1,22 @@
+function promise() {
+    return new Promise((resolve, reject) => {
+        const num = Math.random() > 0.5 ? 1 : 0;
+        if (num) {
+            resolve('PASS');
+        }
+        reject('FAIL')
+    });
+}
+
+
+const arr = [1, 2, 3, 4, 5]
+
+arr.forEach(e => {
+    promise()
+        .then(
+            (success) => {
+                console.log(success);
+            }, (fail) => {
+                console.log(fail);
+            })
+})
