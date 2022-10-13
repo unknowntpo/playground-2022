5c9e911ea327f7fb5f9ebbcc2b24a4beb939e443
張辰謙 <unknowntpo@unknowntpo-MacBook-Pro.local>
2022-10-13 20:00:06 +0800

    import caddy main

---
+++ go/caddy/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	caddycmd "github.com/caddyserver/caddy/v2/cmd"
+
+	// plug in Caddy modules here
+	_ "gihub.com/unknowntpo/playground-2022/go/caddy/helloworld"
+	_ "github.com/caddyserver/caddy/v2/modules/standard"
+)
+
+func main() {
+	caddycmd.Main()
+}
