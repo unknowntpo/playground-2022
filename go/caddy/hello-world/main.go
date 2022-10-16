f2d5acd3782c52cfc8698d7351fdaf65271312e4
unknowntpo <e850506@gmail.com>
2022-10-16 16:39:16 +0800

    rename

--- go/caddy/hello-world/main.go
+++ go/caddy/hello-world/main.go
@@ -4,8 +4,8 @@ import (
 	caddycmd "github.com/caddyserver/caddy/v2/cmd"
 
 	// plug in Caddy modules here
-	_ "gihub.com/unknowntpo/playground-2022/go/caddy/hello-world/helloworld"
 	_ "github.com/caddyserver/caddy/v2/modules/standard"
+	_ "github.com/unknowntpo/playground-2022/go/caddy/hello-world/helloworld"
 )
 
 func main() {
