68030ac02cf20942d0222dc1d4aff18bb9b102ed
unknowntpo <e850506@gmail.com>
2022-10-27 08:22:51 +0800

    16:13: add queryParams button

--- js/wds/postman-clone/script.js
+++ js/wds/postman-clone/script.js
@@ -2,13 +2,19 @@ import "bootstrap"
 import "bootstrap/dist/css/bootstrap.min.css"
 
 const queryParamsContainer = document.querySelector('[data-query-params]')
-const requestHeadersContainer = document.querySelector('query-request-headers]')
+const requestHeadersContainer = document.querySelector('[query-request-headers]')
+const keyValueTemplate = document.querySelector('[data-key-value-template]')
+
+document.querySelector("[data-add-query-param-btn")
+    .addEventListener("click", () => {
+        queryParamsContainer.append(createKeyValuePair())
+    })
 
 queryParamsContainer.append(createKeyValuePair())
 requestHeadersContainer.append(createKeyValuePair())
 
 function createKeyValuePair() {
-    const element = keyValueTemplate.textContent.cloneNode(true)
+    const element = keyValueTemplate.content.cloneNode(true)
     element.querySelector('[data-remove-btn]').addEventListener('click', (e) => {
         e.target.closest("[data-key-value-pair]").remove()
     })
