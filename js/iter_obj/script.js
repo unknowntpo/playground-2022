cb12f85a93ccc74d41be713cb7d3cfc969b45724
unknowntpo <e850506@gmail.com>
2022-10-29 07:44:51 +0800

    Use Object.keys to iterate over object
    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/keys

--- js/iter_obj/script.js
+++ js/iter_obj/script.js
@@ -7,7 +7,17 @@ const obj = {
 const menu = document.querySelector("#menu")
 listProperties(obj)
 
+// https://stackoverflow.com/a/18202926
 function listProperties(obj) {
+    Object.keys(obj).forEach((k, idx) => {
+        let n = document.createElement("ul")
+        n.innerText = `${k}: ${obj[k]}`
+        menu.appendChild(n)
+    })
+}
+
+// https://stackoverflow.com/a/16735184
+function listProperties_old(obj) {
     for (let p in obj) {
         if (Object.prototype.hasOwnProperty.call(obj, p)) {
             let n = document.createElement("ul")
