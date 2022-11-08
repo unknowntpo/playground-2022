e7e4d6a8f9aa2323aaa62b762449d843ef0c0c7c
unknowntpo <e850506@gmail.com>
2022-11-08 12:58:29 +0800

    axios get post request

--- js/axios/request/index.js
+++ js/axios/request/index.js
@@ -2,6 +2,37 @@ const axios = require('axios')
 
 const baseURL = 'https://httpbin.org/'
 
-axios.get(baseURL + "get").then(resp => {
-    console.log(resp.status)
-})
+// axios.get(baseURL + "get").then(resp => {
+//     console.log(resp.status)
+// })
+
+
+async function doGetRequest() {
+    let res = await axios.get(baseURL + "get")
+
+    console.log(Object.keys(res));
+
+    console.log(res.status);
+    let data = res.data;
+    console.log(data);
+}
+
+async function doPostRequest() {
+    const config = {
+        method: 'post',
+        url: baseURL + "post"
+    }
+
+    let res = await axios(config)
+
+    console.log(Object.keys(res));
+
+    console.log(res.status);
+    let data = res.data;
+    console.log(data);
+}
+
+
+
+// doGetRequest();
+doPostRequest();
