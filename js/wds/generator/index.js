85b65256819117ca8a4c117d8118ec43992c715c
unknowntpo <e850506@gmail.com>
2022-11-10 07:52:50 +0800

    add iterator example
    https://youtu.be/IJ6EgdiI_wU?t=536

--- js/wds/generator/index.js
+++ js/wds/generator/index.js
@@ -4,12 +4,24 @@ function* testFn() {
     yield 3
 }
 
+function* Iter(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        yield arr[i]
+    }
+}
+
+// Example 1: basic
 const genFn = testFn()
 
 console.log(genFn.next())
-
+console.log(genFn.next())
+console.log(genFn.next())
 console.log(genFn.next())
 
-console.log(genFn.next())
+// Example 2: iterator
+const iter = Iter([1, 3, 5, 7])
+console.log(iter.next())
+console.log(iter.next())
+console.log(iter.next())
+console.log(iter.next())
 
-console.log(genFn.next())
