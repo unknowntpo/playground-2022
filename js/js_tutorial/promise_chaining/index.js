b1a9f40ec9a379b10614fb6b4dc193fbf4d72d08
unknowntpo <e850506@gmail.com>
2022-11-30 08:26:35 +0800

    Javascript Tutorial: Promise Chaining
    Ref: https://www.javascripttutorial.net/es6/promise-chaining/

---
+++ js/js_tutorial/promise_chaining/index.js
@@ -0,0 +1,34 @@
+function getUser(userId) {
+    return new Promise((resolve, reject) => {
+        console.log('Get user from the database')
+        setTimeout(() => {
+            resolve({
+                userId: userId,
+                username: 'admin'
+            });
+        }, 1000)
+    })
+}
+
+function getServices(user) {
+    return new Promise((resolve, reject) => {
+        console.log(`Get the service of ${user.username} from the datbase`)
+        setTimeout(() => {
+            resolve(['Email', 'VPN', 'CDN'])
+        }, 3 * 1000)
+    })
+}
+
+function getServiceCost(services) {
+    return new Promise((resolve, reject) => {
+        console.log(`Calculate the service cost of ${services}.`);
+        setTimeout(() => {
+            resolve(services.length * 100);
+        }, 2 * 1000);
+    });
+}
+
+getUser(100)
+    .then(getServices)
+    .then(getServiceCost)
+    .then(console.log);
