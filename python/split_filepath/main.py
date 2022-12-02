cd04b43ffd258392259060daf7115f65910ffe02
unknowntpo <e850506@gmail.com>
2022-12-02 08:29:09 +0800

    make output a json string

--- python/split_filepath/main.py
+++ python/split_filepath/main.py
@@ -1,10 +1,18 @@
+import json
+
 def fn(line):
     sep = "/"
     # trim \n and split line by s
     return line.replace("\n", "").split(sep)
 
-with open('files.txt') as f:
+# filename = "files.txt"
+
+filename = "files_less.txt"
+
+with open(filename) as f:
     lines = f.readlines()
     # https://www.pythontutorial.net/python-basics/python-map-list/
     iter = map(fn, lines)
-    print(list(iter))
+    # print(list(iter))
+    str = json.dumps(list(iter))
+    print(str)
