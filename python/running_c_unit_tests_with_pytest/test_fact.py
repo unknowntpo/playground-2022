53baf2c43f3326212f1944f0ebc7e535511a39e0
unknowntpo <e850506@gmail.com>
2022-12-09 07:50:49 +0800

    parametrised tests
    https://p403n1x87.github.io/running-c-unit-tests-with-pytest.html

--- python/running_c_unit_tests_with_pytest/test_fact.py
+++ python/running_c_unit_tests_with_pytest/test_fact.py
@@ -2,11 +2,12 @@ from ctypes import CDLL
 
 import pytest
 
+
 @pytest.fixture
 def libfact():
     yield CDLL("./fact.so")
 
-def test_fact(libfact):
-    assert libfact.fact(6) == 720
-    assert libfact.fact(0) == 1
-    assert libfact.fact(-42) == 1
+
+@pytest.mark.parametrize("n,e", [(6, 720), (0, 1), (-42, 1)])
+def test_fact(libfact, n, e):
+    assert libfact.fact(n) == e
