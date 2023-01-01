db61a88f60be9d7f283027cd908002158c91ec58
unknowntpo <e850506@gmail.com>
2023-01-01 09:02:56 +0800

    HOF: Filter
    https://ithelp.ithome.com.tw/articles/10235812

---
+++ js/ithome/fp-in-js/12-filter-map-reduce/src/hof.js
@@ -0,0 +1,62 @@
+// to do list
+var taskList = [
+    {
+        id: 0,
+        title: 'Buy Avocado',
+        status: 'pending',
+        dueDate: '2020-05-31',
+    }, {
+        id: 1,
+        title: 'Clean house',
+        status: 'complete',
+        dueDate: '2020-05-21',
+    }, {
+        id: 2,
+        title: 'Implement js Array filter',
+        status: 'pending',
+        dueDate: '2019-05-21',
+    }
+];
+
+function Filter(array, fn) {
+    let filterTask = []
+
+    for (let i = 0; i < array.length; i++) {
+        if (fn(array[i])) {
+            filterTask.push(array[i])
+        }
+    }
+    return filterTask
+}
+
+const filterPending = Filter(taskList, list =>
+    list.status === 'pending'
+)
+
+const filterIDis1 = Filter(taskList, list =>
+    list.id === 1
+)
+
+
+describe("filter status='pending and dueDate = '2019-05-21'", () => {
+    test("status='pending'", () => {
+        expect(filterPending).toStrictEqual([].concat(taskList[0], taskList[2]))
+    })
+    test("dueDate = '2019-05-21'", () => {
+        expect(filterIDis1).toStrictEqual([].concat(taskList[1]))
+    })
+})
+
+module.exports = {
+    filterPending,
+    filterIDis1,
+}
+
+function sum(a, b) {
+    return a + b;
+}
+module.exports = sum;
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
+});
