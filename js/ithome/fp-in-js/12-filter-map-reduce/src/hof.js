27fa390e2c28fedb873f9994f021882ce6500a01
unknowntpo <e850506@gmail.com>
2023-01-01 09:14:20 +0800

    remove placeholder jest

--- js/ithome/fp-in-js/12-filter-map-reduce/src/hof.js
+++ js/ithome/fp-in-js/12-filter-map-reduce/src/hof.js
@@ -57,12 +57,3 @@ module.exports = {
     filterPending,
     filterIDis1,
 }
-
-function sum(a, b) {
-    return a + b;
-}
-module.exports = sum;
-
-test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
-});
