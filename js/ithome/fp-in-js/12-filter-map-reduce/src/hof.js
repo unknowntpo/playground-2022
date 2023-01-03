dc308dc56d69f138ebeed2fd9b5433979ae77a5c
unknowntpo <e850506@gmail.com>
2023-01-03 08:26:42 +0800

    add Map function

--- js/ithome/fp-in-js/12-filter-map-reduce/src/hof.js
+++ js/ithome/fp-in-js/12-filter-map-reduce/src/hof.js
@@ -44,6 +44,8 @@ const filterIDis1 = Filter(taskList, list =>
 )
 
 
+
+
 describe("filter status='pending and dueDate = '2019-05-21'", () => {
     test("status='pending'", () => {
         expect(filterPending).toStrictEqual([].concat(taskList[0], taskList[2]))
@@ -52,6 +54,29 @@ describe("filter status='pending and dueDate = '2019-05-21'", () => {
         expect(filterIDis1).toStrictEqual([].concat(taskList[1]))
     })
 })
+
+function Map(array, callback) {
+    let out = []
+
+    array.forEach((e) => {
+        out.push(callback(e))
+    })
+
+    return out
+}
+
+describe("Map", () => {
+    test("apply x+1 to [1,2,3,4]", () => {
+        input = [1, 2, 3, 4]
+        expect(
+            Map(input, (x) => x + 1)
+        ).toStrictEqual([2, 3, 4, 5])
+    })
+})
+
+const count = Map([1, 2, 3], (x) => x + 1)
+
+console.log(count) // => [2, 3, 4]
 
 module.exports = {
     filterPending,
