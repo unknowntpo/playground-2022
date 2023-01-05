5fb2957d54c88f9103ced41ed56c9b22f39a2447
unknowntpo <e850506@gmail.com>
2023-01-05 08:52:41 +0800

    add currying pass fail test

--- js/ithome/fp-in-js/14-currying/src/curry.js
+++ js/ithome/fp-in-js/14-currying/src/curry.js
@@ -15,3 +15,41 @@ describe("addOne", () => {
         ).toEqual(3)
     })
 })
+
+const uncurriedGradeTest = (passGrade, failGrade, average, testScore) => testScore >= average ? passGrade : failGrade;
+
+describe("uncurriedGradeTest", () => {
+    test("FAIL", () => {
+        expect(
+            uncurriedGradeTest('PASS', 'FAIL', 0.2, 0.19)
+        ).toEqual("FAIL")
+    })
+    test("PASS", () => {
+        expect(
+            uncurriedGradeTest('PASS', 'FAIL', 0.2, 0.39)
+        ).toEqual("PASS")
+    })
+})
+
+const getGradeTest = (passGrade, failGrade) => {
+    return (average) => {
+        return (testScore) => {
+            return testScore >= average ? passGrade : failGrade;
+        }
+    }
+}
+
+const passFailTest = getGradeTest('PASS', 'FAIL')(0.2);
+
+describe("curriedGradeTest", () => {
+    test("FAIL", () => {
+        expect(
+            passFailTest(0.19)
+        ).toEqual("FAIL")
+    })
+    test("PASS", () => {
+        expect(
+            passFailTest(0.39)
+        ).toEqual("PASS")
+    })
+})
