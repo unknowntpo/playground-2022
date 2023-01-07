9d74a28257eb65e544e2525b15b3c486f2a967b4
unknowntpo <e850506@gmail.com>
2023-01-07 17:12:14 +0800

    add filter example
    Ref:
    https://ithelp.ithome.com.tw/articles/10235812

--- rust/hof/src/main.rs
+++ rust/hof/src/main.rs
@@ -13,10 +13,26 @@ pub fn map<T: Clone>(arr: Vec<T>, f: fn(e: T) -> T) -> Vec<T> {
     return out;
 }
 
+pub fn filter<T: Clone>(arr: Vec<T>, f: fn(e: T) -> bool) -> Vec<T> {
+    let mut out = Vec::with_capacity(arr.len());
+
+    for e in arr.iter() {
+        if f(e.clone()) {
+            out.push(e.clone())
+        }
+    }
+
+    return out;
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    #[test]
+    fn test_filter() {
+        assert_eq!(filter(vec![1, 2, 3, 4], |x| x % 2 == 0), vec![2, 4]);
+    }
     #[test]
     fn test_map() {
         assert_eq!(map(vec![1, 2, 3, 4], |x| x + 1), vec![2, 3, 4, 5]);
