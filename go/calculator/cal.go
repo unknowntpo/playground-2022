7081c1a49eac8e048ebbb62358a67ea82491c25f
unknowntpo <e850506@gmail.com>
2023-01-12 20:09:40 +0800

    add Token and TokenType

--- go/calculator/cal.go
+++ go/calculator/cal.go
@@ -6,3 +6,18 @@ type Parser struct {
 func NewParser() *Parser {
 	return &Parser{}
 }
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+type TokenType string
+
+const (
+	ADD TokenType = "+"
+	SUB TokenType = "-"
+	MUL TokenType = "*"
+	DIV TokenType = "/"
+	NUM TokenType = "NUM"
+)
