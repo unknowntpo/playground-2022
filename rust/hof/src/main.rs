31d87677347545791f80415cafad02bf439b4611
unknowntpo <e850506@gmail.com>
2023-01-13 08:44:40 +0800

    add hof: currying
    TODO: What is trait ?

--- rust/hof/src/main.rs
+++ rust/hof/src/main.rs
@@ -25,6 +25,16 @@ pub fn filter<T: Clone>(arr: Vec<T>, f: fn(e: T) -> bool) -> Vec<T> {
     return out;
 }
 
+// Ref:
+// https://doc.rust-lang.org/book/ch19-05-advanced-functions-and-closures.html#returning-closures
+pub fn add(x: i32) -> Box<dyn Fn(i32) -> i32> {
+    Box::new(|y| y + 1)
+}
+
+// pub fn add(x: i32) -> Box<dyn Fn(i32) -> i32> {
+//     Box::new(|y| x + y);
+// }
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -33,8 +43,14 @@ mod tests {
     fn test_filter() {
         assert_eq!(filter(vec![1, 2, 3, 4], |x| x % 2 == 0), vec![2, 4]);
     }
+
     #[test]
     fn test_map() {
         assert_eq!(map(vec![1, 2, 3, 4], |x| x + 1), vec![2, 3, 4, 5]);
+    }
+
+    #[test]
+    fn test_add() {
+        assert_eq!(add(1)(2), 3)
     }
 }
