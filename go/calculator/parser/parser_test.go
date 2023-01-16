5da9b543cbef8d2b3c25156436c1d44088aaf6b0
unknowntpo <e850506@gmail.com>
2023-01-16 20:26:02 +0800

    try to implement parser logic

--- go/calculator/parser/parser_test.go
+++ go/calculator/parser/parser_test.go
@@ -15,40 +15,60 @@ func TestParser(t *testing.T) {
 
 var _ = Describe("parser", func() {
 	var (
-		p   *Parser
-		res *Result
+		p      *Parser
+		res    *Result
+		tokens []Token
 	)
-	When("parser.Parse is called", func() {
+
+	When("some token is given", func() {
 		BeforeEach(func() {
-			p = NewParser()
-			res = p.Parse()
-		})
-
-		It("Should return correct parse tree", func() {
-			fmt.Println(res)
-
-			want := &Result{
-				Root: &Node{
-					Token: Token{
-						Type:    ADD,
-						Literal: "+",
-					},
-					Left: &Node{
-						Token: Token{
-							Type:    NUM,
-							Literal: "1",
-						},
-					},
-					Right: &Node{
-						Token: Token{
-							Type:    NUM,
-							Literal: "2",
-						},
-					},
+			tokens = []Token{
+				{
+					Type:    NUM,
+					Literal: "1",
+				},
+				{
+					Type:    ADD,
+					Literal: "+",
+				},
+				{
+					Type:    NUM,
+					Literal: "2",
 				},
 			}
+		})
+		When("parser.Parse is called", func() {
+			BeforeEach(func() {
+				p = NewParser()
+				res = p.Parse(tokens)
+			})
 
-			Expect(res).To(Equal(want))
+			It("Should return correct parse tree", func() {
+				fmt.Println(res)
+
+				want := &Result{
+					Root: &Node{
+						Token: Token{
+							Type:    ADD,
+							Literal: "+",
+						},
+						Left: &Node{
+							Token: Token{
+								Type:    NUM,
+								Literal: "1",
+							},
+						},
+						Right: &Node{
+							Token: Token{
+								Type:    NUM,
+								Literal: "2",
+							},
+						},
+					},
+				}
+
+				Expect(res).To(Equal(want))
+			})
 		})
 	})
 })
