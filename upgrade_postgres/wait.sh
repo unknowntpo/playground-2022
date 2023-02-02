9af136ae805bb5603a581b6d2b90190252a4a2a5
unknowntpo <e850506@gmail.com>
2023-02-02 11:38:23 +0800

    tweak wait.sh use bin inside container to check health of postgres

--- upgrade_postgres/wait.sh
+++ upgrade_postgres/wait.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-status=$(pg_isready -q)
+status=$(docker-compose exec db pg_isready -q)
 while [[ "$status" -ne 0 ]]; do
     echo $status
     sleep 1
-    status=$(pg_isready -q)
+    status=$(docker-compose exec db pg_isready -q)
 done
 
 exit 0
