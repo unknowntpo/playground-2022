25fc32aca509840dd073185ac37a39363fe23428
unknowntpo <e850506@gmail.com>
2023-02-03 16:58:26 +0800

    tweak plannerlog

--- c/pg_extension/plannerlog/plannerlog--1.0.sql
+++ c/pg_extension/plannerlog/plannerlog--1.0.sql
@@ -1,8 +1,8 @@
 CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 
-CREATE FUNCTION plannerlog_hook() RETURNS void
-    AS 'MODULE_PATHNAME'
-    LANGUAGE C;
+-- CREATE FUNCTION plannerlog_hook() RETURNS void
+--     AS 'MODULE_PATHNAME', 'plannerlog_hook'
+--     LANGUAGE C STRICT;
 
 -- Declare the extension
-CREATE EXTENSION plannerlog;
+-- CREATE EXTENSION plannerlog;
--- c/pg_extension/plannerlog/plannerlog.c
+++ c/pg_extension/plannerlog/plannerlog.c
@@ -9,35 +9,43 @@
 PG_MODULE_MAGIC;
 
 /* The hook function */
-static PlannedStmt *
-plannerlog_hook(Query *parse, int cursorOptions, ParamListInfo boundParams)
+PlannedStmt *
+plannerlog_hook(Query *parse, const char *query_string,
+				int cursorOptions,
+				ParamListInfo boundParams)
 {
-    PlannedStmt *stmt;
+	PlannedStmt *stmt;
 
-    /* Call the previous hook */
-    stmt = standard_planner(parse, cursorOptions, boundParams);
+	/* Call the previous hook */
+	// extern PlannedStmt *standard_planner(Query *parse, const char *query_string,
+	// 									 int cursorOptions,
+	// 									 ParamListInfo boundParams);
 
-    /* Log the statement */
-    elog(LOG, "Executing statement: %s", nodeToString(stmt));
+	stmt = standard_planner(parse, query_string, cursorOptions, boundParams);
 
-    return stmt;
+	/* Log the statement */
+	errmsg(LOG, "Executing statement: %s", nodeToString(stmt));
+
+	return stmt;
 }
 
-/* Install the hook */
-void
-_PG_init(void)
-{
-    /* Save the previous hook function */
-    prev_planner_hook = planner_hook;
+PGDLLIMPORT planner_hook_type prev_planner_hook;
 
-    /* Install the hook function */
-    planner_hook = plannerlog_hook;
+void _PG_init(void);
+
+/* Install the hook */
+void _PG_init(void)
+{
+	/* Save the previous hook function */
+	prev_planner_hook = planner_hook;
+
+	/* Install the hook function */
+	planner_hook = plannerlog_hook;
 }
 
 /* Uninstall the hook */
-void
-_PG_fini(void)
+void _PG_fini(void)
 {
-    /* Restore the previous hook function */
-    planner_hook = prev_planner_hook;
+	/* Restore the previous hook function */
+	planner_hook = prev_planner_hook;
 }
