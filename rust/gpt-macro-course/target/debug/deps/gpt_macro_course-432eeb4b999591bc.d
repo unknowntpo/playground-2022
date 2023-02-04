3d5e99fb7883e2788933e2652062b38402938d9d
unknowntpo <e850506@gmail.com>
2023-02-04 21:36:32 +0800

    derived macro by ChatGPT
    https://sharegpt.com/c/AKrvBFC

---
+++ rust/gpt-macro-course/target/debug/deps/gpt_macro_course-432eeb4b999591bc.d
@@ -0,0 +1,5 @@
+/Users/unknowntpo/repo/unknowntpo/playground-2022/rust/gpt-macro-course/target/debug/deps/gpt_macro_course-432eeb4b999591bc.rmeta: src/main.rs
+
+/Users/unknowntpo/repo/unknowntpo/playground-2022/rust/gpt-macro-course/target/debug/deps/gpt_macro_course-432eeb4b999591bc.d: src/main.rs
+
+src/main.rs:
---
+++ rust/gpt-macro-course/target/debug/deps/gpt_macro_course-8705cd8b64334a26.d
@@ -0,0 +1,5 @@
+/Users/unknowntpo/repo/unknowntpo/playground-2022/rust/gpt-macro-course/target/debug/deps/gpt_macro_course-8705cd8b64334a26.rmeta: src/main.rs
+
+/Users/unknowntpo/repo/unknowntpo/playground-2022/rust/gpt-macro-course/target/debug/deps/gpt_macro_course-8705cd8b64334a26.d: src/main.rs
+
+src/main.rs:
