f1f522aeed942cbc3e19f973a473877027a8ae67
unknowntpo <e850506@gmail.com>
2023-02-04 21:25:32 +0800

    max macro generated by ChatGPT
    https://sharegpt.com/c/LzmXYOL

--- c/pg_extension/plannerlog/plannerlog.c
+++ c/pg_extension/plannerlog/plannerlog.c
@@ -5,8 +5,16 @@
 #include "catalog/pg_type.h"
 #include "optimizer/planner.h"
 #include "utils/rel.h"
+#include <stdio.h>
 
 PG_MODULE_MAGIC;
+
+PG_FUNCTION_INFO_V1(query_histogram);
+
+PlannedStmt *
+plannerlog_hook(Query *parse, const char *query_string,
+				int cursorOptions,
+				ParamListInfo boundParams);
 
 /* The hook function */
 PlannedStmt *
@@ -16,17 +24,27 @@ plannerlog_hook(Query *parse, const char *query_string,
 {
 	PlannedStmt *stmt;
 
+	pg_usleep(1000000L);
+
 	/* Call the previous hook */
 	// extern PlannedStmt *standard_planner(Query *parse, const char *query_string,
 	// 									 int cursorOptions,
 	// 									 ParamListInfo boundParams);
 
+	fprintf(stderr, "hello error: %d\n", 1);
+
 	stmt = standard_planner(parse, query_string, cursorOptions, boundParams);
 
 	/* Log the statement */
-	errmsg(LOG, "Executing statement: %s", nodeToString(stmt));
+	ereport(ERROR, "Executing statement: %s", nodeToString(stmt));
 
-	return stmt;
+	ereport(FATAL,
+			(errcode(ERRCODE_UNIQUE_VIOLATION)));
+
+	errbacktrace();
+
+	return NULL;
+	// return stmt;
 }
 
 PGDLLIMPORT planner_hook_type prev_planner_hook;
@@ -36,9 +54,6 @@ void _PG_init(void);
 /* Install the hook */
 void _PG_init(void)
 {
-	/* Save the previous hook function */
-	prev_planner_hook = planner_hook;
-
 	/* Install the hook function */
 	planner_hook = plannerlog_hook;
 }
@@ -47,5 +62,5 @@ void _PG_init(void)
 void _PG_fini(void)
 {
 	/* Restore the previous hook function */
-	planner_hook = prev_planner_hook;
+	planner_hook = NULL;
 }
---
+++ rust/max_macro/target/debug/deps/max_macro-1599f71b0f26342c.d
@@ -0,0 +1,5 @@
+/Users/unknowntpo/repo/unknowntpo/playground-2022/rust/max_macro/target/debug/deps/max_macro-1599f71b0f26342c.rmeta: src/main.rs
+
+/Users/unknowntpo/repo/unknowntpo/playground-2022/rust/max_macro/target/debug/deps/max_macro-1599f71b0f26342c.d: src/main.rs
+
+src/main.rs:
