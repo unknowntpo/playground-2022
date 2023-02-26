7ea0e31ca75f1abaae799c4923acbf3c4040df4f
unknowntpo <e850506@gmail.com>
2023-02-26 19:18:14 +0800

    tweak manager.py add some helper methods to declare, bind queue

--- rabbitmq/job-by-arch/manager/manager.py
+++ rabbitmq/job-by-arch/manager/manager.py
@@ -1,9 +1,20 @@
+import functools
+import logging
 import pika
 import sys
 import os
 import time
 
 exc_name = 'route_job'
+
+
+def log_call(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        logging.info(
+            f"Calling function {func.__name__} with args {args} and kwargs {kwargs}")
+        return func(*args, **kwargs)
+    return wrapper
 
 
 class PikaPublisher:
@@ -16,6 +27,31 @@ class PikaPublisher:
         self.channel.exchange_declare(
             exchange=exchange_name, exchange_type='direct')
 
+        # declare queues
+        self.queue_names = ["aarch64", "amd64"]
+        self.declare_queues(self.queue_names)
+        print("queue declared")
+
+        # Bind queues
+        self.bind_queues(self.queue_names)
+        print("queue binded")
+
+    @log_call
+    def declare_queues(self, queue_names):
+        for q_name in queue_names:
+            print("declaring queue", q_name)
+            result = self.channel.queue_declare(queue=q_name, exclusive=False)
+
+    @log_call
+    def bind_queues(self, queue_names):
+        for q_name in queue_names:
+            print("binding queue", q_name)
+            route_key = q_name
+            self.channel.queue_bind(exchange=self.exchange_name,
+                                    queue=q_name,
+                                    routing_key=route_key)
+
+    @log_call
     def publish(self, message, routing_key):
         self.channel.basic_publish(
             exchange=self.exchange_name,
