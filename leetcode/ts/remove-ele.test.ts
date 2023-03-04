ee1a3c41631520aa51c31cf780bb77206a152a04
unknowntpo <e850506@gmail.com>
2023-03-04 19:54:53 +0800

    add unify ts leetcode test

---
+++ leetcode/ts/remove-ele.test.ts
@@ -0,0 +1,11 @@
+import {describe, expect, test,it} from '@jest/globals';
+
+// https://leetcode.com/problems/remove-element/
+
+describe("test ",()=>{
+  test("1+1",()=>{
+    expect(1+1).toEqual(2)
+  })
+})
+
+
