0fae367562534e55552d8a21dfb168604655b3d1
unknowntpo <e850506@gmail.com>
2023-03-07 15:33:57 +0800

    implement sendDealIfNotSent, markDealAsSent

--- redis/redis-essentials/ts/deal-checking.test.ts
+++ redis/redis-essentials/ts/deal-checking.test.ts
@@ -1,7 +1,8 @@
 // https://github.com/redis/node-redis
 
 // import * as redis from "redis";
-import { createClient, RedisClientType } from "redis";
+import { createClient, RedisClientType, RedisFlushModes } from "redis";
+import { isMainThread } from "worker_threads";
 // const RedisClient = redis;
 
 
@@ -35,10 +36,49 @@ describe("deal-checking", () => {
     console.log("ping ok")
     expect(1 + 1).toEqual(2)
   })
+
+  test("if deal has already sent to the user", async () => {
+    // client.ping
+    await client.ping()
+    console.log("ping ok")
+
+    const dealID: string = "deal:1"
+    const userID: string = "1"
+
+    await sendDealIfNotSent(client, dealID, userID)
+
+    const isMember: boolean = await client.sIsMember(dealID, userID)
+    expect(isMember).toEqual(true)
+  })
 })
 
-// function markDealAsSent(dealId, userId) {
-//   client.sadd(dealId, userId)
+
+// class dealSender {
+//   dealID: {}
+//   client: RedisClientType
+
+//   constructor(dealID: number, client: RedisClientType) {
+//     this.dealID = dealID
+//     this.client = client
+//   }
+
+
+//   public sendDealIfNotSent(dealID: , userID) {
+
+//   }
 // }
+
+async function sendDealIfNotSent(client: RedisClientType, dealID: string, userID: string) {
+  const isMember: boolean = await client.sIsMember(dealID, userID)
+  if (isMember) {
+    console.log(`Deal ${dealID} was already sent to user ${userID}`)
+    return
+  }
+  markDealAsSent(client, dealID, userID)
+}
+
+function markDealAsSent(client: RedisClientType, dealID: string, userID: string) {
+  client.sAdd(dealID, userID)
+}
 
 
