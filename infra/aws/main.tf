8fea3dd7408c921c828ba295299adc0976405d08
unknowntpo <e850506@gmail.com>
2023-03-07 19:48:31 +0800

    add aws example terraform file

--- infra/aws/main.tf
+++ infra/aws/main.tf
@@ -2,19 +2,29 @@
 provider "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
-  region     = "us-west-2"
+  region     = "ap-northeast-1"
 }
+
+variable "aws_access_key" {
+  type = string
+}
+
+variable "aws_secret_key" {
+  type = string
+}
+
+
 
 # Define the EC2 instance resource.
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0" # Specify the ID of the Amazon Machine Image you want to use.
+  ami           = "ami-00eeedc4036573771" # Ubuntu 22.04.1 LTS (GNU/Linux 5.15.0-1028-aws x86_64)
   instance_type = "t2.micro"              # Specify the instance type.
 
   # Define the connection information to connect to the EC2 instance.
   connection {
     type        = "ssh"
     user        = "ubuntu"
-    private_key = file("~/.ssh/my_key.pem") # Specify the path to your SSH private key.
+    private_key = file("~/.ssh/aws-key.pem") # Specify the path to your SSH private key.
     host        = self.public_ip
   }
 
@@ -24,15 +34,15 @@ resource "aws_instance" "example" {
   }
 }
 
-# Define the Route 53 record.
-resource "aws_route53_record" "unknowntpo.net" {
-  name    = "unknowntpo.net"
-  type    = "A"
-  zone_id = "Z1EXAMPLEZONEID" # Specify the ID of your Route 53 hosted zone.
+# # Define the Route 53 record.
+# resource "aws_route53_record" "unknowntpo.net" {
+#   name    = "unknowntpo.net"
+#   type    = "A"
+#   zone_id = "Z1EXAMPLEZONEID" # Specify the ID of your Route 53 hosted zone.
 
-  # Specify the EC2 instance IP address as the record value.
-  records = [aws_instance.example.public_ip]
+#   # Specify the EC2 instance IP address as the record value.
+#   records = [aws_instance.example.public_ip]
 
-  # Set a TTL of 300 seconds.
-  ttl = 300
-}
+#   # Set a TTL of 300 seconds.
+#   ttl = 300
+# }
