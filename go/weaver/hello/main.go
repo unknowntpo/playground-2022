dc31616129a8543d9719748113217d17578a2764
unknowntpo <e850506@gmail.com>
2023-04-11 15:21:17 +0800

    add listener

--- go/weaver/hello/main.go
+++ go/weaver/hello/main.go
@@ -4,14 +4,14 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"net/http"
 
 	"github.com/ServiceWeaver/weaver"
 )
 
 func main() {
 	// Initialize the Service Weaver application.
-	ctx := context.Background()
-	root := weaver.Init(ctx)
+	root := weaver.Init(context.Background())
 
 	// Get a client to the Reverser component.
 	reverser, err := weaver.Get[Reverser](root)
@@ -19,10 +19,22 @@ func main() {
 		log.Fatal(err)
 	}
 
-	// Call the Reverse method.
-	reversed, err := reverser.Reverse(ctx, "!dlroW ,olleH")
+	// Get a network listener on address "localhost:12345".
+	opts := weaver.ListenerOptions{LocalAddress: "localhost:12345"}
+	lis, err := root.Listener("hello", opts)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(reversed)
+	fmt.Printf("hello listener available on %v\n", lis)
+
+	// Serve the /hello endpoint.
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		reversed, err := reverser.Reverse(r.Context(), r.URL.Query().Get("name"))
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		fmt.Fprintf(w, "Hello, %s!\n", reversed)
+	})
+	http.Serve(lis, nil)
 }
