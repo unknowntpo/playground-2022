c6590ea2b11824989b3d91448a1debbebdd44f28
unknowntpo <e850506@gmail.com>
2023-05-10 17:32:35 +0800

    leetcode(palindrome-partitioning): failed impl

--- leetcode/ts/131-palindrome-partitioning.test.ts
+++ leetcode/ts/131-palindrome-partitioning.test.ts
@@ -3,10 +3,35 @@ import { describe, expect, test, it } from '@jest/globals';
 
 // https://leetcode.com/problems/palindrome-partitioning/
 function partition(s: string): string[][] {
-  return [["a"]]
+  let out: string[][] = [[]]
+  dp(s, 0, out)
+  return out
 };
 
+function dp(s: string, startIdx: number, out: string[][]) {
+  if (s.length == 0) return
+  if (s.slice(0, startIdx).split("").reverse().join("") !== s) return
+  for (let i = startIdx; i < s.length; i++) {
+    dp(s.slice(0, i), i, out)
+  }
+}
 /*
+aab
+
+a
+a
+b
+
+aa
+aab (x
+
+aab
+
+(x)
+
+aa
+
+
 aab
 
 a 
