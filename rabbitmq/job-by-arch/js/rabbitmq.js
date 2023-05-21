0f511b1dab5d14ee1ec1fce7b5d973aeded43eaa
unknowntpo <e850506@gmail.com>
2023-05-21 18:49:02 +0800

    add example of using RabbitMQ as producer and consumer

---
+++ rabbitmq/job-by-arch/js/rabbitmq.js
@@ -0,0 +1,65 @@
+const amqp = require('amqplib')
+
+const url = 'amqp://127.0.0.1'
+
+const queue = 'my_queue';
+
+async function produce(producerName) {
+  while (1) {
+    try {
+      const connection = await amqp.connect(url);
+      const channel = await connection.createChannel()
+
+      const message = 'Hello, RabbitMQ'
+
+      await channel.assertQueue(queue)
+      await channel.sendToQueue(queue, Buffer.from(message))
+
+      console.log(`${producerName}: message sent:`, message)
+
+      await channel.close()
+      await connection.close()
+    } catch (error) {
+      console.error(`${producerName} error:`, error)
+    }
+    await sleep(1000)
+  }
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function consume(consumerName) {
+  while (1) {
+    try {
+      const connection = await amqp.connect(url);
+      const channel = await connection.createChannel();
+
+      await channel.assertQueue(queue);
+      await channel.consume(queue, (message) => {
+        console.log(`Consummer ${consumerName} received message:`, message.content.toString());
+        channel.ack(message);
+      });
+      console.log(`${consumerName}: Waiting for messages...`);
+    } catch (error) {
+      console.error(`${consumerName} error:`, error);
+    }
+    await sleep(1000)
+  }
+}
+
+async function start() {
+  try {
+    await Promise.all([
+      produce('Producer 1'),
+      consume('Consummer 1'),
+      consume('Consummer 1')
+    ])
+  } catch (error) {
+    console.error('Error: ', error)
+  }
+}
+
+start()
+
