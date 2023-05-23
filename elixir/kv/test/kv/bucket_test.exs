da2c47b4b3ea789eb2b1b01ce22d86c3065fdc97
unknowntpo <e850506@gmail.com>
2023-05-23 14:47:04 +0800

    (elixir: KV): Add delete method to bucket

--- elixir/kv/test/kv/bucket_test.exs
+++ elixir/kv/test/kv/bucket_test.exs
@@ -12,4 +12,13 @@ defmodule KV.BucketTest do
     KV.Bucket.put(bucket, "milk", 3)
     assert KV.Bucket.get(bucket, "milk") == 3
   end
+
+  test "delete a key", %{bucket: bucket} do
+    assert KV.Bucket.get(bucket, "milk") == nil
+
+    KV.Bucket.put(bucket, "milk", 3)
+
+    KV.Bucket.delete(bucket, "milk")
+    assert KV.Bucket.get(bucket, "milk") == nil
+  end
 end
