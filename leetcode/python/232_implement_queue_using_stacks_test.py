b026a4a2b8b6b05d9f168eaea15531636c70b630
unknowntpo <e850506@gmail.com>
2023-05-30 21:40:56 +0800

    implement post-order traversal with visitied stack

--- leetcode/python/232_implement_queue_using_stacks_test.py
+++ leetcode/python/232_implement_queue_using_stacks_test.py
@@ -11,22 +11,53 @@ class TreeNode:
 
 class Solution:
     def postOrderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        return []
+        if root is None:
+            return []
+        visited = [False]
+        stack = [root]
+        out = []
+        while stack:
+            node = stack.pop()
+            v = visited.pop()
+            if v is True:
+                out.append(node.val)
+            else:
+                stack.append(node)
+                visited.append(True)
+                if node.right is not None:
+                    stack.append(node.right)
+                    visited.append(False)
+                if node.left is not None:
+                    stack.append(node.left)
+                    visited.append(False)
+        return out
 
 
 testCases = [
     {
+        "name": "Tree: [1]",
+        "root": TreeNode(1),
+        "want": [1],
+    },
+    {
+        "name": "Tree: [1, 2, 3]",
+        "root": TreeNode(1, TreeNode(2), TreeNode(3)),
+        "want": [2, 3, 1],
+    },
+    {
+        "name": "Tree: [1, x, 2, 3 ,x]",
         "root": TreeNode(1, None, TreeNode(2, TreeNode(3))),
         "want": [3, 2, 1],
     },
     {
+        "name": "Tree: []",
         "root": None,
         "want": [],
     },
 ]
 
 
-@pytest.mark.parametrize("testCase", testCases)
+@pytest.mark.parametrize("testCase", testCases, ids=lambda testCase: testCase["name"])
 def test_postOrderTraversal(testCase):
     sol = Solution()
     root = testCase["root"]
