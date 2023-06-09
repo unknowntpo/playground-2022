e7830a04483ec5740f7621652a90083cc73b5ae3
unknowntpo <e850506@gmail.com>
2023-06-09 14:39:02 +0800

    (leetcode: java) WIP: add twoSum template

---
+++ leetcode/java/README.md
@@ -0,0 +1,1 @@
+https://chat.openai.com/share/7e1982bd-59b2-4856-a135-a6becb09c677
---
+++ leetcode/java/app/src/main/java/leetcode/TwoSum.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+  public int[] TwoSum(int[] nums,  int target) {
+    Map<Integer, Integer> numToIndexMap = new HashMap<>();
+
+    for (int i = 0; i < nums.length;i++) {
+      int complement = target - nums[i];
+      if (numToIndexMap.containsKey(complement)){
+        return new int[]{numToIndexMap.get(complement), i};
+      }
+      numToIndexMap.put(nums[i], i);
+    }
+        return new int[]{};
+  }
+};
