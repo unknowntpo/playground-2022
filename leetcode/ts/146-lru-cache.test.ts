9d02052ec9b0f4983bec4f78d27c4d413f5ea021
unknowntpo <e850506@gmail.com>
2023-06-12 09:33:27 +0800

    (leetcode: 146_lru_cache) WIP: fix tests
    TODO: How to avoid update index in this.map while modifying list

--- leetcode/ts/146-lru-cache.test.ts
+++ leetcode/ts/146-lru-cache.test.ts
@@ -10,52 +10,63 @@ class LRUCache<K, V> {
   map: Map<K, number>
   list: Array<KeyValue<K, V>>
   cap: number
-  constructor() {
+  constructor(cap: number) {
+    // map from key to index of this key in list
     this.map = new Map<K, number>()
     this.list = []
+    this.cap = cap
   }
 
   get(key: K): V | undefined {
     if (this.map.has(key)) {
       let idx: number = this.map.get(key)!
-      this.moveToFront(idx)
-      return this.list[0].value
+      this.moveToBack(idx)
+      // update its index to 0
+      this.map.set(key, 0)
+      return this.list[this.list.length - 1].value
     }
     return undefined
   }
 
-  moveToFront(idx: number) {
+  /*
+* [{1, 1}, {2, 2}]
+*
+* */
+
+  moveToBack(idx: number) {
     let kv = this.list.splice(idx, 1)[0]
-    this.list.unshift(kv)
+    this.list.push(kv)
+    this.map.set(kv.key, this.list.length - 1)
   }
 
   put(key: K, value: V) {
     if (this.map.has(key)) {
       // find the index of the key in list, move it to back of list  
       let idx: number = this.map.get(key)!
-      this.moveToFront(idx)
+      this.moveToBack(idx)
     } else {
       // try to append to list
       if (this.list.length == this.cap) {
         // got head
+        // evict head first
         let head = this.list.shift()
         this.map.delete(head!.key)
-      } else {
-        this.list.push({ key: key, value: value })
       }
+      this.list.push({ key: key, value: value })
+      this.map.set(key, this.list.length - 1)
     }
   }
 }
 
 describe(`normal`, () => {
-  let cache = new LRUCache()
+  let cache = new LRUCache(2)
   cache.put(1, 1)
   cache.put(2, 2)
   expect(cache.get(1)).toEqual(1)
   cache.put(3, 3)
-  expect(cache.get(2)).toEqual(-1)
+  expect(cache.get(2)).toEqual(undefined)
   cache.put(4, 4)
-  expect(cache.get(1)).toEqual(-1)
+  expect(cache.get(1)).toEqual(undefined)
   expect(cache.get(3)).toEqual(3)
   expect(cache.get(4)).toEqual(4)
 })
