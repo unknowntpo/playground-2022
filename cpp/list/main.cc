70a3956c3b0723655ecb6ddf7390b1df6d0db323
unknowntpo <e850506@gmail.com>
2023-06-16 08:00:59 +0800

    (cpp: list) list::splice: move target to front

--- cpp/list/main.cc
+++ cpp/list/main.cc
@@ -3,6 +3,14 @@
 
 using namespace std;
 
+template <typename T> void print_list(list<T> *pList) {
+  list<T> l = *pList;
+  for (auto it = l.begin(); it != l.end(); it++) {
+    cout << "e: " << *it << "\t";
+  }
+  cout << endl;
+}
+
 int main(int argc, char *argv[]) {
   list<string> l;
 
@@ -13,9 +21,14 @@ int main(int argc, char *argv[]) {
   l.push_front("z");
   l.push_front("x");
 
-  auto front_it = l.begin();
+  string target = "a";
+  auto a_it = l.begin();
 
   for (auto it = l.begin(); it != l.end(); it++) {
+    // find target
+    if (*it == target) {
+      a_it = it;
+    }
     cout << "e: " << *it << "\t";
   }
 
@@ -24,6 +37,14 @@ int main(int argc, char *argv[]) {
   for (auto it = l.rbegin(); it != l.rend(); it++) {
     cout << "e: " << *it << "\t";
   }
+
+  // find 'a', move it to front
+  //
+  l.splice(l.begin(), l, a_it);
+
+  cout << "\nafter moving target " << target << " to front: " << endl;
+
+  print_list(&l);
 
   return 0;
 }
