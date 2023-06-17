4109d58d2b9d1775e0400dc52fef2945d8c05935
unknowntpo <e850506@gmail.com>
2023-06-18 07:44:49 +0800

    (cpp: reverse_iterator): Try to figure out property of list::rbegin().base()

---
+++ cpp/reverse_iter/main.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <list>
+#include <unordered_map>
+
+int main(int argc, char *argv[]) {
+  std::unordered_map<int, std::list<int>::iterator> um;
+
+  std::list<int> l;
+
+  for (int i = 0; i < 6; i++) {
+    l.push_back(i);
+    um[i] = l.rbegin().base();
+  }
+
+  for (const auto &n : l) {
+    std::cout << n << std::endl;
+  }
+
+  std::cout << "in um: " << std::endl;
+
+  for (auto it = um.begin(); it != um.end(); it++) {
+    std::cout << "key: " << it->first << " value: " << *it->second
+              << " addr of iterator: " << &(it) << std::endl;
+  }
+  return 0;
+}
