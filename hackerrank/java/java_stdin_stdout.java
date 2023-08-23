f773ea9ab20471351adcea0fa2752638e0eb9192
unknowntpo <e850506@gmail.com>
2023-08-23 10:20:55 +0800

    (hackerank: java) stdin_stdout: refactor: use ArrayList

--- hackerrank/java/java_stdin_stdout.java
+++ hackerrank/java/java_stdin_stdout.java
@@ -5,12 +5,12 @@ public class Solution {
   public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
 
-    int a = scan.nextInt();
-    int b = scan.nextInt();
-    int c = scan.nextInt();
+    ArrayList<Integer> l = new ArrayList<Integer>();
 
-    System.out.println(a);
-    System.out.println(b);
-    System.out.println(c);
+    for (Integer i = 0; i < 3;i++) {
+      l.add(scan.nextInt());
+    }
+
+    l.forEach(e -> System.out.println(e));
   }
 }
