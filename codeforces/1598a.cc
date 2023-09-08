a6c4865cc771aedada53464e3c4a29f831a95e5c
unknowntpo <e850506@gmail.com>
2023-09-08 08:10:07 +0800

    (cf: 1598a) See answer
    https://www.youtube.com/watch?v=adqmqWyNylc

--- codeforces/1598a.cc
+++ codeforces/1598a.cc
@@ -2,28 +2,19 @@
 #include <iostream>
 #include <vector>
 
-#define DEBUG 0
-
 using namespace std;
 
-int solve(vector<vector<int>> &grid, int colsNum) {
+string solve(string in[], int numCols) {
+  string ans = "YES";
 
-  grid[1][1] = 1;
-
-  for (int i = 1; i < 3; i++) {
-    for (int j = 1; j <= colsNum; j++) {
-      // cout << "i: " << i << " j: " << j << endl;
-      if (i == 1 && j == 1)
-        continue;
-      if (grid[i][j] == 1) {
-        grid[i][j] = 0;
-        continue;
-      } else {
-        grid[i][j] = grid[i - 1][j] + grid[i][j - 1] + grid[i - 1][j - 1];
-      }
+  cout << numCols << endl;
+  for (int j = 1; j < numCols; j++) {
+    if (in[0][j] == '1' and in[1][j] == '1') {
+      ans = "NO";
+      return ans;
     }
   }
-  return grid[2][colsNum];
+  return ans;
 }
 
 int main() {
@@ -33,42 +24,15 @@ int main() {
   int tc = 1;
   cin >> tc;
   for (int t = 1; t <= tc; t++) {
-    cout << "Case #" << t << ": ";
-    int colsNum = 0;
-    cin >> colsNum;
-    vector<vector<int>> grid(3, vector<int>(colsNum + 1, 0));
+    cout << "Case #" << t << ": " << endl;
+    int numCols = 0;
+    cin >> numCols;
 
-    for (int i = 1; i < 3; i++) {
-      string in;
-      cin >> in;
-      int j = 1;
-      for (char c : in) {
-        int num = c - '0'; // Convert char to int
-        grid[i][j++] = num;
-      }
-    }
-    int res = solve(grid, colsNum);
-    if (DEBUG) {
-      cout << "result: " << res << endl;
-    }
-    string out = "";
-    if (res > 0) {
-      out = "YES";
-    } else {
-      out = "NO";
-    }
-    cout << out << endl;
-    if (DEBUG) {
-      // cout << format("grid {}", grid);
-      //     // display
-      cout << "--------DEBUG------" << endl;
-      for (int i = 1; i < 3; i++) {
-        for (int j = 1; j <= colsNum; j++) {
-          cout << grid[i][j];
-        }
-        cout << endl;
-      }
-      cout << "--------DEBUG END------" << endl;
-    }
+    string in[2];
+    for (int i = 0; i < 2; i++)
+      cin >> in[i];
+
+    string res = solve(in, numCols);
+    cout << res << endl;
   }
 }
