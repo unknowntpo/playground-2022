7876310e7819a45a168e901666e32f41c1125ee0
unknowntpo <e850506@gmail.com>
2023-09-18 09:55:47 +0800

    (ruby:ihower:unit test) leap_year
    https://ihower.tw/rails/fullstack-testing-rspec.html

---
+++ ruby/rails/ihower/unit_test/leap_year/leap_year.rb
@@ -0,0 +1,15 @@
+def is_leap_year?(year)
+  if year % 4 == 0
+    if year % 100 == 0
+      if year % 400 == 0
+        return true
+      else
+        return false
+      end
+    else
+      return true
+    end
+  else
+    return false
+  end
+end
