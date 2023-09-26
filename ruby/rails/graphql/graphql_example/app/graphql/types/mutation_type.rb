56eae5b00382f1ec42a7097237811815a6eca6ce
unknowntpo <e850506@gmail.com>
2023-09-26 14:12:48 +0800

    (graphql) add mutation

--- ruby/rails/graphql/graphql_example/app/graphql/types/mutation_type.rb
+++ ruby/rails/graphql/graphql_example/app/graphql/types/mutation_type.rb
@@ -2,9 +2,7 @@ module Types
   class MutationType < Types::BaseObject
     # TODO: remove me
     field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World"
-    end
+                               description: 'An example field added by the generator'
+    field :create_user, mutation: Mutations::CreateUser
   end
 end
