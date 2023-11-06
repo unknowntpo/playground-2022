b564757955ce27ce0455a44fbabcc8f4397ae605
unknowntpo <e850506@gmail.com>
2023-11-06 10:04:49 +0800

    Rust:Tokio:Spawning:Accepting sockets
    https://tokio.rs/tokio/tutorial/spawning

--- rust/tokio/my-redis/src/main.rs
+++ rust/tokio/my-redis/src/main.rs
@@ -1,14 +1,22 @@
-use mini_redis::{client, Result};
+use tokio::net::{TcpListener, TcpStream};
+use mini_redis::{Connection, Frame};
 
 #[tokio::main]
-async fn main() -> Result<()> {
-    let mut client = client::connect("127.0.0.1:6379").await?;
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+    loop {
+        let (socket, _) = listener.accept().await.unwrap();
+        process(socket).await;
+    }
+}
 
-    client.set("hello", "world".into()).await?;
+async fn process(socket: TcpStream) {
+    let mut connection = Connection::new(socket);
 
-    let result = client.get("hello").await?;
+    if let Some(frame) = connection.read_frame().await.unwrap() {
+        println!("GOT: {:?}", frame);
 
-    println!("got value from the server, result = {:?}", result);
-
-    Ok(())
+        let response = Frame::Error("unimplemented".to_string());
+        connection.write_frame(&response).await.unwrap();
+    }
 }
