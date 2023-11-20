95c1e8a8504069d3781caf164284729e3b393171
unknowntpo <e850506@gmail.com>
2023-11-20 19:23:57 +0800

    validate url

--- rust/rust_first_class/httpie/src/main.rs
+++ rust/rust_first_class/httpie/src/main.rs
@@ -1,10 +1,12 @@
-
 use clap::Parser;
+use std::str::FromStr;
+use anyhow::{anyhow, Result};
+use reqwest::Url;
 
 // Ref: https://github.com/tyrchen/geektime-rust/blob/master/04_httpie/src/main.rs
 
 #[derive(Parser, Debug)]
-#[clap(version= "1.0", author = "xxx")]
+#[clap(version = "1.0", author = "xxx")]
 struct Opts {
     #[clap(subcommand)]
     subcmd: SubCommand,
@@ -23,11 +25,41 @@ struct Get {
 
 #[derive(Parser, Debug)]
 struct Post {
+    #[clap(parse(try_from_str = parse_url))]
     url: String,
-    body: Vec<String>,
+    #[clap(parse(try_from_str=parse_kv_pair))]
+    body: Vec<KvPair>,
+}
+
+#[derive(Debug)]
+struct KvPair {
+    k: String,
+    v: String,
+}
+
+impl FromStr for KvPair {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut split = s.split("=");
+        let err = || anyhow!(format!("Failed to parse {}", s));
+        Ok(Self {
+            k: (split.next().ok_or_else(err)?).to_string(),
+            v: (split.next().ok_or_else(err)?).to_string(),
+        })
+    }
+}
+
+fn parse_kv_pair(s: &str) -> Result<KvPair> {
+    Ok(s.parse()?)
+}
+
+fn parse_url(s: &str) -> Result<String> {
+    let _url: Url = s.parse()?;
+    Ok(s.into())
 }
 
 fn main() {
-    let opts: Opts= Opts::parse();
+    let opts: Opts = Opts::parse();
     println!("{:?}", opts);
 }
