7167bc819b67c4098fdadb336acee6fc453427a2
unknowntpo <e850506@gmail.com>
2023-12-07 07:50:50 +0800

    use anyhow to accept different type of Result<()>

--- rust/error/anyhow_exp/src/error.rs
+++ rust/error/anyhow_exp/src/error.rs
@@ -1,3 +1,5 @@
+use anyhow::Result;
+use std::fs;
 use thiserror::Error;
 
 #[derive(Error, Debug)]
@@ -6,8 +8,7 @@ pub enum SuperError {
     SuperErrorSideKick(String),
 }
 
-pub fn get_super_error() -> Result<(), SuperError> {
-    Err(SuperError::SuperErrorSideKick(
-        "why error? Side kick!".to_string(),
-    ))
+pub fn get_super_error() -> Result<()> {
+    fs::read_to_string("non-exist")?;
+    Err(SuperError::SuperErrorSideKick("why error? Side kick!".to_string()).into())
 }
