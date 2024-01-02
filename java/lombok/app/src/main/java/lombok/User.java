58b942c5b3fd964e591f75affe6bc79f3ef5bc2b
unknowntpo <e850506@gmail.com>
2024-01-02 20:08:04 +0800

    (java:lombok) add example

---
+++ java/lombok/app/src/main/java/lombok/User.java
@@ -0,0 +1,16 @@
+package lombok;
+
+@ToString
+@Getter
+@Setter
+public class User {
+    private Integer id;
+    private String name;
+    private String address;
+
+    public User(Integer id, String name, String address) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+    }
+}
