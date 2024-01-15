ac2029ef033a9619ca11f9a728db147a6e0eaac4
unknowntpo <e850506@gmail.com>
2024-01-15 14:27:00 +0800

    add moi.html

--- js/react/tic_tac_toe/src/App.js
+++ js/react/tic_tac_toe/src/App.js
@@ -14,7 +14,7 @@ class Square extends React.Component {
   render() {
     return (
       <button className="square"
-        onClick={() => { this.setState({ value: 'X' }) }}
+        onClick={() => { this.props.onClick() }}
       >
         {this.state.value}
       </button >
@@ -23,8 +23,17 @@ class Square extends React.Component {
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    }
+  }
   renderSquare(i) {
-    return <Square value={i} />;
+    return <Square
+      value={this.state.squares[i]}
+      onClick={() => this.handleClick(i)}
+    />;
   }
 
   render() {
--- ts/cheerio/lawsnote_crawler/src/index.ts
+++ ts/cheerio/lawsnote_crawler/src/index.ts
@@ -2,69 +2,112 @@ import * as cheerio from 'cheerio';
 import * as fs from 'fs';
 
 
-console.log("hello")
+function test() {
+    const html = fs.readFileSync('index.html', 'utf8');
+    const $ = cheerio.load(html)
 
-const html = fs.readFileSync('index.html', 'utf8');
-const $ = cheerio.load(html)
+    console.log(`Title ${$('title').text()}`)
 
-console.log(`Title ${$('title').text()}`)
+    // const $trs = $('table.table > tbody > tr:not(:first-child)');
 
-// const $trs = $('table.table > tbody > tr:not(:first-child)');
+    const $trs = $('table.table > tbody > tr');
 
-const $trs = $('table.table > tbody > tr');
+    // console.log(`rows ${$trs}`)
 
-// console.log(`rows ${$trs}`)
+    // $trs.each((_i, tr) => {
+    //   const $tr = $(tr);
+    //   const publishedDateText = $tr.find('td:nth-child(2)').text().trim();
+    //   const hasAbolishableTag = $tr.find('td:nth-child(3) > span').hasClass('label-fei');
+    //   const link = $tr.find('td:nth-child(3) > a').attr('href');
+    //   const title = $tr.find('td:nth-child(3) > a').text().trim();
+    //   const type = $tr.find('td:nth-child(4)').text().trim();
 
-// $trs.each((_i, tr) => {
-//   const $tr = $(tr);
-//   const publishedDateText = $tr.find('td:nth-child(2)').text().trim();
-//   const hasAbolishableTag = $tr.find('td:nth-child(3) > span').hasClass('label-fei');
-//   const link = $tr.find('td:nth-child(3) > a').attr('href');
-//   const title = $tr.find('td:nth-child(3) > a').text().trim();
-//   const type = $tr.find('td:nth-child(4)').text().trim();
+    //   if (!link) {
+    //     throw new Error('無法取得函釋連結');
+    //   }
 
-//   if (!link) {
-//     throw new Error('無法取得函釋連結');
-//   }
+    //   const rawItpRuleEntry: RawItpRuleEntry = {
+    //     publishedDateText,
+    //     hasAbolishableTag,
+    //     url: new URL(link, url).toString(),
+    //     extraInfos: [
+    //       {
+    //         sourceId: this.sourceId,
+    //         ['法規類別']: type,
+    //       },
+    //     ],
+    //     title,
+    //   };
+    //   rawItpRuleEntriesByUrl.push(rawItpRuleEntry);
+    // });
 
-//   const rawItpRuleEntry: RawItpRuleEntry = {
-//     publishedDateText,
-//     hasAbolishableTag,
-//     url: new URL(link, url).toString(),
-//     extraInfos: [
-//       {
-//         sourceId: this.sourceId,
-//         ['法規類別']: type,
-//       },
-//     ],
-//     title,
-//   };
-//   rawItpRuleEntriesByUrl.push(rawItpRuleEntry);
-// });
+    type Row = {
+        ID: number,
+        Date: string,
+        Title: string,
+        Type: string,
+    }
 
-type Row = {
-    ID: number,
-    Date: string,
-    Title: string,
-    Type: string,
+    const rows: Array<Row> = [];
+
+    $trs.each((_i, tr) => {
+        const $tr = $(tr)
+        const id = Number($tr.find('td:nth-child(1)').text().trim().slice(0, -1));
+        const date = $tr.find('td:nth-child(2)').text();
+        const title = $tr.find('td:nth-child(3) > a').text().trim();
+        const type = $tr.find('td:nth-child(4)').text().trim();
+
+        // console.log($tr.text())
+        rows.push({
+            ID: id,
+            Date: date,
+            Title: title,
+            Type: type,
+        })
+    })
+
+    console.log(`rows: ${JSON.stringify(rows, null, '\t')}`)
 }
 
-const rows: Array<Row> = [];
+function moi() {
+    const html = fs.readFileSync('moi.html', 'utf8');
+    const $ = cheerio.load(html)
 
-$trs.each((_i, tr) => {
-    const $tr = $(tr)
-    const id = Number($tr.find('td:nth-child(1)').text().trim().slice(0, -1));
-    const date = $tr.find('td:nth-child(2)').text();
-    const title = $tr.find('td:nth-child(3) > a').text().trim();
-    const type = $tr.find('td:nth-child(4)').text().trim();
+    console.log(`Title ${$('title').text()}`)
 
-    // console.log($tr.text())
-    rows.push({
-        ID: id,
-        Date: date,
-        Title: title,
-        Type: type,
+    // const $trs = $('table.table > tbody > tr:not(:first-child)');
+
+    const lis = $('div#tree.well.leftCategory > ul > li');
+
+    console.log(`length: ${lis.length}`)
+
+    type Row = {
+        // ID: number,
+        // Date: string,
+        Title: string,
+        // Type: string,
+    }
+
+    const rows: Array<Row> = [];
+
+    lis.each((_i, li) => {
+        const list = $(li)
+        // const id = Number($tr.find('td:nth-child(1)').text().trim().slice(0, -1));
+        // const date = $tr.find('td:nth-child(2)').text();
+        const title = list.find('a').text().trim();
+        // const type = $tr.find('td:nth-child(4)').text().trim();
+
+        // console.log($tr.text())
+        rows.push({
+            // ID: id,
+            // Date: date,
+            Title: title,
+            // Type: type,
+        })
     })
-})
+    // console.log(`rows: ${JSON.stringify(rows, null, '\t')}`)
+    console.log(`categories: ${JSON.stringify(rows, null, "\t")}`)
+}
 
-console.log(`rows: ${JSON.stringify(rows, null, '\t')}`)
+// test()
+moi()
