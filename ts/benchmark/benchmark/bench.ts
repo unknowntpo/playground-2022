9a11353c93b1760e17878d45a3db9be2c34c4da7
unknowntpo <e850506@gmail.com>
2024-01-24 08:35:37 +0800

    ts:benchmark: add fib_iter and fib suite

---
+++ ts/benchmark/benchmark/bench.ts
@@ -0,0 +1,38 @@
+import Benchmark from "benchmark";
+
+const fib = (i: number): number => {
+    if (i < 0) return 0;
+    if (i == 0 || i == 1) return i;
+    return fib(i-1) + fib(i-2);
+}
+
+const fib_iter = (n: number) : number => {
+if (n < 0) return 0;
+    if (n == 0 || n == 1) return n;
+    let pre = 0;
+    let cur = 1;
+    for (let i = 2; i < n; i++) {
+        const nxt = pre + cur;
+        pre = cur;
+        cur = nxt;
+    }
+    return cur; 
+}
+
+let suite = new Benchmark.Suite;
+
+// Adding a test to the suite
+suite.add('fib', () => {
+    fib(10); // You can change the number to test different inputs
+})
+suite.add('fib_iter', () => {
+    fib_iter(10); // You can change the number to test different inputs
+})
+.on('cycle', (event: Benchmark.Event) => {
+    console.log(String(event.target));
+})
+.on('complete', function() {
+    console.log('Fastest is ' + suite.filter('fastest').map('name'));
+})
+.run({ 'async': true });
+
