61f6dbe59419f423f4843e00624efaea233b4f8f
unknowntpo <e850506@gmail.com>
2024-01-26 14:59:53 +0800

    feat(rs:proxy): done proxying: todo: add tests

--- rust/proxy/examples/server.rs
+++ rust/proxy/examples/server.rs
@@ -2,50 +2,59 @@ use anyhow::Result;
 use std::future::Future;
 use std::io::{copy, Error as IOError};
 use std::net::SocketAddr;
+use tokio::io::AsyncWriteExt;
 use tokio::{
     io::AsyncReadExt,
     net::{TcpListener, TcpStream},
 };
 use tracing::*;
+use tracing_subscriber::*;
+use url::Url;
 
 use proxy::proxy::Error;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    std::env::set_var("RUST_LOG", "trace");
-
+    tracing_subscriber::fmt::init();
     let addr = "localhost:4000";
     let listener = TcpListener::bind(addr).await?;
     loop {
         let (socket, addr) = listener.accept().await?;
         tokio::spawn(async move {
-            println!("got connection");
+            trace!("got connection");
             process(socket, addr).await.unwrap();
         });
     }
 }
 
-async fn process(mut socket: TcpStream, addr: SocketAddr) -> Result<(), IOError> {
+// https://proxiesapi.com/articles/how-to-build-a-simple-http-proxy-in-rust-in-just-40-lines
+async fn process(mut socket: TcpStream, addr: SocketAddr) -> Result<()> {
     let mut buffer = [0; 1024];
     let n = socket.read(&mut buffer).await?;
 
     let request = String::from_utf8_lossy(&buffer[..n]);
 
-    // let url = request.split_whitespace();
+    let relative_url = request
+        .split_whitespace()
+        .nth(1)
+        .expect("should have 1th element");
 
-    println!("Request: {}", request);
+    // Construct the full URL
+    let full_url = format!("http://{}{}", addr, relative_url);
 
-    // let mut res = reqwest::get(url);
+    trace!("{}", full_url);
 
-    // copy(&mut res, &mut socket)?;
+    // Parse the full URL
+    let parsed_url = Url::parse(&full_url).expect("Failed to parse the URL");
 
+    // let parsed_url = Url::parse(url).expect("Failed to parse the URL");
+
+    if let Some(query) = parsed_url.query() {
+        for (_, dest_url) in url::form_urlencoded::parse(query.as_bytes()) {
+            let res = reqwest::get(dest_url.into_owned()).await?.bytes().await?;
+            info!("{:?}", res);
+            socket.write(&res).await?;
+        }
+    }
     Ok(())
 }
-
-// fn handle_client(mut stream: TcpStream, addr: SocketAddr) -> impl Future<Output = Result<()>> {
-//     async move {
-//         // ... handle client logic
-//         stream.send
-//         Ok(())
-//     }
-// }
