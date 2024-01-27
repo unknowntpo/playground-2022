7c662bd24a03daace8fc7c9e776a514fb37a365a
unknowntpo <e850506@gmail.com>
2024-01-27 20:13:13 +0800

    gcloud:profiler:node: add skaffold

--- gcp/cloud_profiler/node/src/index.ts
+++ gcp/cloud_profiler/node/src/index.ts
@@ -1,38 +1,48 @@
 import * as profiler from '@google-cloud/profiler';
+import { Hono } from 'hono';
+import { serve } from '@hono/node-server'
+import { logger } from 'hono/logger';
 
 const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms))
 
-const slowFunc = async () => {
-  await sleep(100)
-  await slowFunc2()
+const slowFunc = (n: number) => slowFunc2(n);
+
+const slowFunc2 = (n: number) => slowFunc3(n);
+
+const slowFunc3 = (n: number) => fib(n)
+
+const fib = (n: number): number => {
+  if (n <= 0) return 0;
+  if (n <= 1) return n;
+  return fib(n - 1) + fib(n - 2);
 }
-
-const slowFunc2 = async () => {
-  await sleep(300)
-  await slowFunc3()
-}
-
-const slowFunc3 = async () => {
-  await sleep(500)
-  for (let i = 0; i < 500; i++) {
-    console.log("Done")
-  }
-}
-
-
 
 async function main() {
+  console.log(`test: ${process.env.TEST}`)
+  console.log(`process.env.GCP_CLIENT_EMAIL: ${process.env.GCP_CLIENT_EMAIL}`)
+  console.log(`process.env.GCP_PRIVATE_KEY: ${process.env.GCP_PRIVATE_KEY}`)
   await profiler.start({
-    projectId: 'web-service-design',
+    projectId: process.env.PROJECT_ID,
     serviceContext: {
-      service: 'slowFuncService',
+      service: 'package-json-image',
       version: '1.0.0',
     },
+    credentials: {
+      client_email: process.env.GCP_CLIENT_ID,
+      private_key: process.env.GCP_CLIENT_SECRET
+    },
+    logLevel: 4,
   });
-  for (let i = 0; i < 500; i++) {
-    await slowFunc()
-    console.log("Done")
-  }
+  const app = new Hono();
+  app.use('*', logger());
+  app.get('/', (c) => c.text('Hono🔥'));
+  app.get('/slow/:n', async (c) => {
+    const n = Number(c.req.param('n'));
+    const res = slowFunc(n);
+    return c.text(`fib(${n}) = ${res}`);
+  });
+
+  serve({ fetch: app.fetch, port: 4444 })
 }
 
 main()
