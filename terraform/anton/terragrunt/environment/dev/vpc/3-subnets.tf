ae82703c79952b7b1f5bfacd3f540411e0b1addc
unknowntpo <e850506@gmail.com>
2024-03-08 20:09:27 +0800

    feat(terraform/anton): commit files

---
+++ terraform/anton/terragrunt/environment/dev/vpc/3-subnets.tf
@@ -0,0 +1,23 @@
+resource "aws_subnet" "private_us_east_1a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.0.0/19"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    "Name"                            = "dev-private-us-east-1a"
+    "kubernetes.io/role/internal-elb" = 1
+    "kubernetes.io/cluster/dev-demo"  = "owned"
+  }
+}
+
+resource "aws_subnet" "private_us_east_1b" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.32.0/19"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    "Name"                            = "dev-private-us-east-1b"
+    "kubernetes.io/role/internal-elb" = 1
+    "kubernetes.io/cluster/dev-demo"  = "owned"
+  }
+}
