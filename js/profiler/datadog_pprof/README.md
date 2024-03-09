64546247c2db022704997084b36304d01fc3fe17
unknowntpo <e850506@gmail.com>
2024-03-09 08:22:52 +0800

    feat(js/profile/node_inspector): add node_inspector

--- js/profiler/datadog_pprof/README.md
+++ js/profiler/datadog_pprof/README.md
@@ -1,1 +1,17 @@
-$ pprof -http=: heap.pb.gz
+
+
+In a shell:
+
+```
+$ yarn profile
+```
+
+Other shell:
+
+```
+$ sh -c 'for i in {1..40}; do curl http://localhost:9090/30; done'
+```
+
+And then go to your default chrome.
+
+type  `about:inspect` in search bar, and click "Open dedicated DevTool for Node"
--- js/profiler/datadog_pprof/src/index.ts
+++ js/profiler/datadog_pprof/src/index.ts
@@ -8,12 +8,21 @@ const app = express();
 router.get('/:n', (req, res) => {
   const n = parseInt(req.params.n);
   const result = fib(n);
+  mem(n);
   res.json({ "result": result });
 });
 
 function fib(n: number): number {
   if (n < 2) return 1;
   return fib(n - 1) + fib(n - 2);
+}
+
+function mem(n: number) {
+  const hugeArray = new Array<Array<string>>(n);
+
+  for (let i = 0; i < hugeArray.length; i++) {
+    hugeArray.push(new Array(n / 10).fill('some string')); // Nested arrays
+  }
 }
 
 app.use(router);
