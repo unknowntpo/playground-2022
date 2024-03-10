3265676d334fc7155be46262cece3915ffe508d7
unknowntpo <e850506@gmail.com>
2024-03-10 19:12:36 +0800

    feat(js/profile/node-inspector-test): add node-profiler-next

--- js/profiler/node-inspector-test/src/index.ts
+++ js/profiler/node-inspector-test/src/index.ts
@@ -1,6 +1,31 @@
 import { abc } from "src/moduleA.js";
 import express from 'express';
 
+import fs from 'fs';
+import v8Profiler from 'v8-profiler-next';
+const title = 'good-name';
+
+// set generateType 1 to generate new format for cpuprofile
+// to be compatible with cpuprofile parsing in vscode.
+v8Profiler.setGenerateType(1);
+
+// ex. 5 mins cpu profile
+v8Profiler.startProfiling(title, true);
+setTimeout(() => {
+  const profile = v8Profiler.stopProfiling(title);
+  profile.export(function(error, result) {
+    // if it doesn't have the extension .cpuprofile then
+    // chrome's profiler tool won't like it.
+    // examine the profile:
+    //   Navigate to chrome://inspect
+    //   Click Open dedicated DevTools for Node
+    //   Select the profiler tab
+    //   Load your file
+    fs.writeFileSync(`${title}.cpuprofile`, result);
+    profile.delete();
+  });
+}, 5 * 1000);
+
 const router = express.Router();
 const port = 9090;
 const app = express();
@@ -8,7 +33,7 @@ const app = express();
 router.get('/:n', (req, res) => {
   const n = parseInt(req.params.n);
   const result = fib(n);
-  mem(n);
+  // mem();
   res.json({ "result": result });
 });
 
@@ -21,7 +46,7 @@ function mem(n: number) {
   const hugeArray = new Array<Array<string>>(n);
 
   for (let i = 0; i < hugeArray.length; i++) {
-    hugeArray.push(new Array(n / 10).fill('some string')); // Nested arrays
+    hugeArray.push(new Array(Math.round(n / 10) + 1).fill('some string')); // Nested arrays
   }
 }
 
