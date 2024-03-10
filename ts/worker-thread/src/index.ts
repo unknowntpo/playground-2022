8ecc81e3cad19e4bc3fc2bbbf6abe5c1aa1d1975
unknowntpo <e850506@gmail.com>
2024-03-10 20:43:17 +0800

    feat(ts/worker-thread): use google cloud profiler

--- ts/worker-thread/src/index.ts
+++ ts/worker-thread/src/index.ts
@@ -1,33 +1,21 @@
 import { Worker } from 'worker_threads';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import v8Profiler from 'v8-profiler-next';
-const title = 'good-name';
-import fs from 'fs';
-
-// set generateType 1 to generate new format for cpuprofile
-// to be compatible with cpuprofile parsing in vscode.
-v8Profiler.setGenerateType(1);
-
-// ex. 5 mins cpu profile
-v8Profiler.startProfiling(title, true);
-setTimeout(() => {
-    const profile = v8Profiler.stopProfiling(title);
-    profile.export(function (error, result) {
-        // if it doesn't have the extension .cpuprofile then
-        // chrome's profiler tool won't like it.
-        // examine the profile:
-        //   Navigate to chrome://inspect
-        //   Click Open dedicated DevTools for Node
-        //   Select the profiler tab
-        //   Load your file
-        fs.writeFileSync(`${title}.cpuprofile`, result as string);
-        profile.delete();
-    });
-}, 5 * 1000);
+import profiler from '@google-cloud/profiler';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+
+await profiler.start({
+  projectId: 'web-service-design',
+  serviceContext: {
+    service: 'web-service-design-cloud-profiler-worker-thread',
+    version: '1.3.0',
+  },
+  sourceMapSearchPath: ['.'],
+});
+
 
 // Create a new worker
 const worker = new Worker(path.resolve(__dirname, 'worker.js'));
@@ -37,34 +25,34 @@ worker.postMessage('Hello, Worker!');
 
 // Listen for messages from the worker
 worker.on('message', async (result) => {
-    console.log(`Main thread received result: ${result}`);
-    for (let i = 0; i < 10000; i++) {
-        concat();
-        await sleep(10);
-    }
-    console.log('wait for worker to be terminated')
-    await worker.terminate();
+  console.log(`Main thread received result: ${result}`);
+  for (let i = 0; i < 10000; i++) {
+    concat();
+    await sleep(10);
+  }
+  console.log('wait for worker to be terminated')
+  await worker.terminate();
 });
 
 const sleep = async (ms: number) => {
-    return new Promise<void>((resolve) => {
-        setTimeout(() => resolve(), ms)
-    })
+  return new Promise<void>((resolve) => {
+    setTimeout(() => resolve(), ms)
+  })
 }
 
 function concat() {
-    let s = '';
-    for (let i = 0; i < 1000; i++) {
-        s += 'hello';
-    }
+  let s = '';
+  for (let i = 0; i < 100000; i++) {
+    s += 'hello';
+  }
 }
 
 // Listen for any errors that occur in the worker thread
 worker.on('error', (error) => {
-    console.error('Worker error:', error);
+  console.error('Worker error:', error);
 });
 
 // Listen for when the worker stops
 worker.on('exit', (code) => {
-    console.log(`Worker stopped with exit code ${code}`);
+  console.log(`Worker stopped with exit code ${code}`);
 });
