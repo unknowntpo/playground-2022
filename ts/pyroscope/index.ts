0ae14eb0833ab789fdb5bd5749b0fa68c8ccdcbf
unknowntpo <e850506@gmail.com>
2024-03-22 15:58:44 +0800

    feat(ts:pyroscope): fix

--- ts/pyroscope/index.ts
+++ ts/pyroscope/index.ts
@@ -1,37 +1,59 @@
 import * as Pyroscope from '@pyroscope/nodejs';
 
 const slowFunc = async () => {
-  await sleep(100)
-  slowFunc2()
+  await slowFunc2()
 }
 
 const slowFunc2 = async () => {
-  await sleep(300)
-  slowFunc3()
+  await concat()
 }
 
-const slowFunc3 = async () => {
-  await sleep(500)
-  for (let i = 0; i < 500; i++) {
-    console.log("Done")
+async function concat() {
+  let str = "";
+  for (let i = 0; i < 10000; i++) {
+    for (let j = 0; j < 5000; j++) {
+      str += "hello";
+    }
   }
 }
 
 
 const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms))
 
+function fib(n: number): number {
+  if (n < 2) return n;
+  return fib(n - 1) + fib(n - 2);
+}
+
 async function main() {
   Pyroscope.init({
-    serverAddress: 'http://localhost:4040',
+    serverAddress: 'http://127.0.0.1:4040',
     appName: 'myNodeService'
   });
 
+  console.log("Starting pyroscope");
+
   Pyroscope.start()
 
-  for (let i = 0; i < 500; i++) {
+  console.log("Counting fib");
+
+  fib(40);
+
+  console.log("Concatinating string");
+
+  for (let i = 0; i < 1; i++) {
     await slowFunc()
     console.log("Done")
   }
+
+  console.log("Stopping pyroscope");
+
+  Pyroscope.stop();
+
+  console.log("Pyroscope stopped");
 }
 
 main()
+
+
+
