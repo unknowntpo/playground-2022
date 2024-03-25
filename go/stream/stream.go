2111d41360c1eaa2f3523e64156ad12c2981dc90
unknowntpo <e850506@gmail.com>
2024-03-25 17:35:09 +0800

    feat(go:stream): finish impl except Map method

--- go/stream/stream.go
+++ go/stream/stream.go
@@ -18,7 +18,9 @@ type Stream[T, U any] interface {
 	Next() (T, bool)
 	Produce(T)
 	Map(func(T) U) Stream[T, U]
-	Reduce(reduceFn func(accumulator U, target T) U, initValue U) U
+	Reduce(reduceFn func(target T, accumulator U) U, initValue U) U
+	FromSlice(slice []T) Stream[T, U]
+	Close()
 }
 
 type stream[T, U any] struct {
@@ -31,13 +33,45 @@ func (s *stream[T, U]) Next() (T, bool) {
 }
 
 func (s *stream[T, U]) Produce(ele T) {
+	s.ch <- ele
 }
 
-func (s *stream[T, U]) Map(func(T) U) Stream[T, U] {
-	return nil
+func (s *stream[T, U]) FromSlice(slice []T) Stream[T, U] {
+	go func() {
+		for _, e := range slice {
+			s.Produce(e)
+		}
+		close(s.ch)
+	}()
+	return s
 }
-func (s *stream[T, U]) Reduce(reduceFn func(accumulator U, target T) U, initValue U) U {
-	return initValue
+
+func (s *stream[T, U]) Close() {
+	close(s.ch)
+}
+
+func (s *stream[T, U]) Map(mapFn func(T) U) Stream[T, U] {
+	next := NewStream[T, U]()
+	go func() {
+		for e := range s.ch {
+			next.Produce(mapFn(e))
+		}
+		next.Close()
+	}()
+	return next
+}
+
+func (s *stream[T, U]) Reduce(reduceFn func(target T, accumulator U) U, initValue U) U {
+	acc := initValue
+	for e := range s.ch {
+		acc = reduceFn(e, acc)
+	}
+	return acc
+}
+
+func NewStream[T, U any]() Stream[T, U] {
+	stream := &stream[T, U]{ch: make(chan T, 10)}
+	return stream
 }
 
 /*
--- go/stream/stream_test.go
+++ go/stream/stream_test.go
@@ -1,1 +1,15 @@
 package stream
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestStream(t *testing.T) {
+	stream := NewStream[int, int]()
+	result := stream.FromSlice([]int{1, 2, 3}).
+		Map(func(e int) int { return e * 2 }).
+		Reduce(func(e int, acc int) int { return acc + e }, 0)
+	assert.Equal(t, 12, result)
+}
