fb692d0b8e6ae84becbacf568ed0b1318be11ef5
unknowntpo <e850506@gmail.com>
2024-03-26 08:40:06 +0800

    feat(go:stream): Use Map with type params

--- go/stream/stream.go
+++ go/stream/stream.go
@@ -1,5 +1,7 @@
 package stream
 
+import "fmt"
+
 /*
 
 map: Stream[T] -> fn([T]) T -> Stream[T]
@@ -14,46 +16,47 @@ forEach: Stream[T] -> fn[T]
 */
 
 // T: target, U: accumulator
-type Stream[T, U any] interface {
+type Stream[T any] interface {
 	Next() (T, bool)
 	Produce(T)
-	Map(func(T) U) Stream[T, U]
-	Reduce(reduceFn func(target T, accumulator U) U, initValue U) U
-	FromSlice(slice []T) Stream[T, U]
+	Reduce(reduceFn func(target T, accumulator T) T, initValue T) T
+	FromSlice(slice []T) Stream[T]
 	Close()
 }
 
-type stream[T, U any] struct {
-	ele T
-	ch  chan T
+type stream[T any] struct {
+	ch chan T
 }
 
-func (s *stream[T, U]) Next() (T, bool) {
-	return s.ele, false
+func (s *stream[T]) Next() (T, bool) {
+	e, more := <-s.ch
+	return e, more
 }
 
-func (s *stream[T, U]) Produce(ele T) {
-	s.ch <- ele
+func (s *stream[T]) Produce(e T) {
+	s.ch <- e
 }
 
-func (s *stream[T, U]) FromSlice(slice []T) Stream[T, U] {
+func (s *stream[T]) FromSlice(slice []T) Stream[T] {
 	go func() {
 		for _, e := range slice {
+			fmt.Println("FromSlice: produce e ", e)
 			s.Produce(e)
 		}
-		close(s.ch)
+		s.Close()
 	}()
 	return s
 }
 
-func (s *stream[T, U]) Close() {
+func (s *stream[T]) Close() {
 	close(s.ch)
 }
 
-func (s *stream[T, U]) Map(mapFn func(T) U) Stream[T, U] {
-	next := NewStream[T, U]()
+func Map[T, U any](s Stream[T], mapFn func(T) U) Stream[U] {
+	next := NewStream[U]()
 	go func() {
-		for e := range s.ch {
+		for e, more := s.Next(); more; {
+			fmt.Println("Map: got e ", e)
 			next.Produce(mapFn(e))
 		}
 		next.Close()
@@ -61,16 +64,17 @@ func (s *stream[T, U]) Map(mapFn func(T) U) Stream[T, U] {
 	return next
 }
 
-func (s *stream[T, U]) Reduce(reduceFn func(target T, accumulator U) U, initValue U) U {
+func (s *stream[T]) Reduce(reduceFn func(target T, accumulator T) T, initValue T) T {
 	acc := initValue
-	for e := range s.ch {
+	for e, more := s.Next(); more; {
+		fmt.Println("got e, ", e)
 		acc = reduceFn(e, acc)
 	}
 	return acc
 }
 
-func NewStream[T, U any]() Stream[T, U] {
-	stream := &stream[T, U]{ch: make(chan T, 10)}
+func NewStream[T any]() Stream[T] {
+	stream := &stream[T]{ch: make(chan T, 10)}
 	return stream
 }
 
--- go/stream/stream_test.go
+++ go/stream/stream_test.go
@@ -7,9 +7,11 @@ import (
 )
 
 func TestStream(t *testing.T) {
-	stream := NewStream[int, int]()
-	result := stream.FromSlice([]int{1, 2, 3}).
-		Map(func(e int) int { return e * 2 }).
+	stream := NewStream[int]()
+	result := Map(
+		stream.FromSlice([]int{1, 2, 3}),
+		func(e int) int { return e * 2 },
+	).
 		Reduce(func(e int, acc int) int { return acc + e }, 0)
 	assert.Equal(t, 12, result)
 }
