3e642db749ddaa7544f6dedd5a8579d94e500e79
unknowntpo <e850506@gmail.com>
2024-03-27 08:49:31 +0800

    fix: make for loop works

--- go/stream/stream.go
+++ go/stream/stream.go
@@ -2,19 +2,6 @@ package stream
 
 import "fmt"
 
-/*
-
-map: Stream[T] -> fn([T]) T -> Stream[T]
-map: func[T, U](input Stream[T], mapFn func(T) U)) Stream[U]
-
-reduce: Stream[T] -> reduceFn[T] -> T
-
-forEach: Stream[T] -> fn[T]
-
-// return error ?
-
-*/
-
 // T: target, U: accumulator
 type Stream[T any] interface {
 	Next() (T, bool)
@@ -43,7 +30,9 @@ func (s *stream[T]) FromSlice(slice []T) Stream[T] {
 			fmt.Println("FromSlice: produce e ", e)
 			s.Produce(e)
 		}
+		fmt.Println("FromSlice: closing s")
 		s.Close()
+		fmt.Println("FromSlice: closed s")
 	}()
 	return s
 }
@@ -55,7 +44,7 @@ func (s *stream[T]) Close() {
 func Map[T, U any](s Stream[T], mapFn func(T) U) Stream[U] {
 	next := NewStream[U]()
 	go func() {
-		for e, more := s.Next(); more; {
+		for e, more := s.Next(); more; e, more = s.Next() {
 			fmt.Println("Map: got e ", e)
 			next.Produce(mapFn(e))
 		}
@@ -66,8 +55,8 @@ func Map[T, U any](s Stream[T], mapFn func(T) U) Stream[U] {
 
 func (s *stream[T]) Reduce(reduceFn func(target T, accumulator T) T, initValue T) T {
 	acc := initValue
-	for e, more := s.Next(); more; {
-		fmt.Println("got e, ", e)
+	for e, more := s.Next(); more; e, more = s.Next() {
+		fmt.Println("Reduce got e, ", e)
 		acc = reduceFn(e, acc)
 	}
 	return acc
--- go/stream/stream_test.go
+++ go/stream/stream_test.go
@@ -7,11 +7,14 @@ import (
 )
 
 func TestStream(t *testing.T) {
-	stream := NewStream[int]()
-	result := Map(
-		stream.FromSlice([]int{1, 2, 3}),
-		func(e int) int { return e * 2 },
-	).
-		Reduce(func(e int, acc int) int { return acc + e }, 0)
-	assert.Equal(t, 12, result)
+	t.Run("test map-reduce", func(t *testing.T) {
+		slice := []int{1, 2, 3}
+		stream := NewStream[int]()
+		result := Map(
+			stream.FromSlice(slice),
+			func(e int) int { return e * 2 },
+		).
+			Reduce(func(e int, acc int) int { return acc + e }, 0)
+		assert.Equal(t, 12, result)
+	})
 }
