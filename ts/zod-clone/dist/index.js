a23468e76dc7c03acdaf9127a3c30a411154c1f7
unknowntpo <e850506@gmail.com>
2024-04-09 06:50:33 +0800

    feat: isString, isNumber

--- ts/zod-clone/dist/index.js
+++ ts/zod-clone/dist/index.js
@@ -2,3 +2,17 @@
 
 // index.ts
 console.log("hello");
+function isnt(val, typeName) {
+  return new Error(`${val} is not a ${typeName}`);
+}
+function isString(val) {
+  if (typeof val !== "string")
+    throw isnt(val, "string");
+}
+function isNumber(val) {
+  if (typeof val !== "number")
+    throw isnt(val, "number");
+}
+console.log(`is string: ${isString("hello")}`);
+console.log(`is number: ${isNumber(123)}`);
+console.log(`is number: ${isNumber("NaN")}`);
--- ts/zod-clone/index.ts
+++ ts/zod-clone/index.ts
@@ -1,1 +1,22 @@
 console.log("hello")
+
+function isnt(val: unknown, typeName: string) {
+  return new Error(`${val} is not a ${typeName}`);
+}
+
+
+function isString(val: unknown): asserts val is string {
+  if (typeof val !== "string") throw isnt(val, "string");
+}
+
+function isNumber(val: unknown): asserts val is number {
+  if (typeof val !== "number") throw isnt(val, "number");
+}
+
+console.log(`is string: ${isString("hello")}`)
+//console.log(`is string: ${isString(123)}`)
+
+
+console.log(`is number: ${isNumber(123)}`)
+
+// console.log(`is number: ${isNumber("NaN")}`)
