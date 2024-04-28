3460e6563e7869ea59421bda5d883ddf75960f9d
unknowntpo <e850506@gmail.com>
2024-04-29 06:48:33 +0800

    feat(parser_combinator): use parser combinator to compose digit, hexDigit

--- ts/parser_combinator/src/index.test.ts
+++ ts/parser_combinator/src/index.test.ts
@@ -2,35 +2,35 @@ import { describe, expect, test, it } from '@jest/globals';
 import { char, either, digit } from '.';
 
 describe('char', () => {
-    it('match a single character', () => {
-        expect(char('a')('abc')).toBeTruthy();
-    })
-    it('fails matching if first character does not match', () => {
-        expect(char('a')('bc')).toBeFalsy();
-    })
+  it('match a single character', () => {
+    expect(char('a')('abc')).toBeTruthy();
+  })
+  it('fails matching if first character does not match', () => {
+    expect(char('a')('bc')).toBeFalsy();
+  })
 })
 
 describe("either", () => {
-    const aOrB = either(char("a"), char("b"));
+  const aOrB = either(char("a"), char("b"));
 
-    it("returns true if any of the combinators are true", () => {
-        expect(aOrB("a")).toBeTruthy();
-        expect(aOrB("b")).toBeTruthy();
-    });
+  it("returns true if any of the combinators are true", () => {
+    expect(aOrB("a")).toBeTruthy();
+    expect(aOrB("b")).toBeTruthy();
+  });
 
-    it("returns false none of the combinators are true", () => {
-        expect(aOrB("c")).toBeFalsy();
-    });
+  it("returns false none of the combinators are true", () => {
+    expect(aOrB("c")).toBeFalsy();
+  });
 });
 
 
 describe("digit", () => {
-    it("return true when input is digit", () => {
-        expect(digit("036937530354")).toBeTruthy();
-        expect(digit("038438593343")).toBeTruthy();
-    });
+  it("return true when input is digit", () => {
+    expect(digit("036937530354")).toBeTruthy();
+    expect(digit("038438593343")).toBeTruthy();
+  });
 
-    it("returns false when input is not digit", () => {
-        expect(digit("0c74759322545")).toBeFalsy();
-    });
+  it("returns false when input is not digit", () => {
+    expect(digit("abcde")).toBeFalsy();
+  });
 });
--- ts/parser_combinator/src/index.ts
+++ ts/parser_combinator/src/index.ts
@@ -1,27 +1,21 @@
 type Combinator = (str: string) => boolean;
 
 export const char = (c: string): Combinator => {
-    return (str: string) => {
-        return (str[0] === c);
-    }
+  return (str: string) => {
+    return (str[0] === c);
+  }
 }
 
 export const either = (...combinators: Combinator[]): Combinator => {
-    return (s: string) => {
-        for (let i = 0; i < combinators.length; i++) {
-            if (combinators[i](s)) {
-                return true;
-            }
-        }
-        return false;
+  return (s: string) => {
+    for (let i = 0; i < combinators.length; i++) {
+      if (combinators[i](s)) {
+        return true;
+      }
     }
-}
-
-export const digit = (s: string): Combinator => {
-  for (let i = 0; i < s.length;i++) {
-    const code = s.charCodeAt(i);
-    if (!(code >= 48 && code <= 57)) return false;
+    return false;
   }
-  return true;
 }
 
+export const digit = either(..."0123456789".split('').map(char));
+export const hexDigit = either(digit, ..."abcdefABCDEF".split('').map(char));
