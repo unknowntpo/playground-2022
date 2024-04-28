0e7883ab5a38d8d9f34005ae6156ea6b12a690aa
unknowntpo <e850506@gmail.com>
2024-04-29 07:02:42 +0800

    feat(parser_combinator): add failed example of sequence

--- ts/parser_combinator/src/index.test.ts
+++ ts/parser_combinator/src/index.test.ts
@@ -1,12 +1,18 @@
 import { describe, expect, test, it } from '@jest/globals';
-import { char, either, digit } from '.';
+import { char, either, digit, sequence } from '.';
 
 describe('char', () => {
   it('match a single character', () => {
-    expect(char('a')('abc')).toBeTruthy();
+    expect(char('a')('abc')).toEqual({
+      success: true,
+      value: "abc",
+      rest: "bc"
+    });
   })
   it('fails matching if first character does not match', () => {
-    expect(char('a')('bc')).toBeFalsy();
+    expect(char('a')('bc')).toEqual({
+      success: false,
+    });
   })
 })
 
@@ -14,23 +20,61 @@ describe("either", () => {
   const aOrB = either(char("a"), char("b"));
 
   it("returns true if any of the combinators are true", () => {
-    expect(aOrB("a")).toBeTruthy();
-    expect(aOrB("b")).toBeTruthy();
+    expect(aOrB("a")).toEqual({
+      success: true,
+      value: "a",
+      rest: "",
+    });
+    expect(aOrB("b")).toEqual({
+      success: true,
+      value: "b",
+      rest: ""
+    });
   });
 
   it("returns false none of the combinators are true", () => {
-    expect(aOrB("c")).toBeFalsy();
+    expect(aOrB("c")).toEqual({
+      success: false
+    });
   });
 });
 
 
 describe("digit", () => {
   it("return true when input is digit", () => {
-    expect(digit("036937530354")).toBeTruthy();
-    expect(digit("038438593343")).toBeTruthy();
+    expect(digit("036937530354")).toEqual({
+      success: true,
+      value: "036937530354",
+      rest: "36937530354"
+    });
+    expect(digit("038438593343")).toEqual({
+      success: true,
+      value: "038438593343",
+      rest: "38438593343"
+    });
   });
 
   it("returns false when input is not digit", () => {
-    expect(digit("abcde")).toBeFalsy();
+    expect(digit("abcde")).toEqual({
+      success: false
+    });
   });
 });
+
+describe("sequence", () => {
+  const aAndB = sequence(char("a"), char("b"));
+
+  it("returns true if all of the combinators are true", () => {
+    expect(aAndB("abcdef")).toEqual({
+      success: true,
+      value: "ab",
+      rest: "cdef"
+    });
+  });
+
+  it("returns false any of the combinators are false", () => {
+    expect(aAndB("bcdef")).toEqual({
+      success: false
+    });
+  });
+})
--- ts/parser_combinator/src/index.ts
+++ ts/parser_combinator/src/index.ts
@@ -1,20 +1,59 @@
-type Combinator = (str: string) => boolean;
+interface SuccessResult {
+  success: true;
+  value: string;
+  rest: string;
+}
+
+interface FailureResult {
+  success: false;
+}
+
+type CombinatorResult = SuccessResult | FailureResult;
+type Combinator = (str: string) => CombinatorResult;
 
 export const char = (c: string): Combinator => {
   return (str: string) => {
-    return (str[0] === c);
+    if (str[0] === c) {
+      return { success: true, value: str, rest: str.substring(1) }
+    } else {
+      return { success: false }
+    }
   }
 }
 
 export const either = (...combinators: Combinator[]): Combinator => {
   return (s: string) => {
     for (let i = 0; i < combinators.length; i++) {
-      if (combinators[i](s)) {
-        return true;
+      const res = combinators[i](s);
+      if (res.success) {
+        return res;
       }
     }
-    return false;
+    return { success: false };
   }
+}
+
+export function sequence(...combinators: Combinator[]): Combinator {
+  return (str: string) => {
+    let rest = str;
+    let value = "";
+
+    for (let i = 0; i < combinators.length; i++) {
+      const result = combinators[i](rest);
+      if (result.success) {
+        rest = result.rest;
+        value += result.value;
+      } else {
+        return { success: false };
+      }
+    }
+
+    return {
+      success: true,
+      value,
+      rest
+    };
+  };
 }
 
 export const digit = either(..."0123456789".split('').map(char));
