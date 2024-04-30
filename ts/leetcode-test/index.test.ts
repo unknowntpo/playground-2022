b1e975c328b6054f7b4b3d5f8444f19ad7f0a74b
unknowntpo <e850506@gmail.com>
2024-04-30 15:42:05 +0800

    feat(ts:test): make test pass

--- ts/leetcode-test/index.test.ts
+++ ts/leetcode-test/index.test.ts
@@ -25,15 +25,15 @@ describe('testtest', () => {
 		const parser = new Parser(rawData);
 		const res = parser.parse();
 		expect(res).toEqual({
-			stratum: 0,
-			list: [
+			"stratum": "paragraphs",
+			"list": [
 				{
-					content: "法規條文應分條書寫，冠以「第某條」字樣，並得分為項、款、目。項不冠數字，空二字書寫，款冠以一、二、三等數字，目冠以（一）、（二）、（三）等數字，並應加具標點符號。",
+					"content": "法規條文應分條書寫，冠以「第某條」字樣，並得分為項、款、目。項不冠數字，空二字書寫，款冠以一、二、三等數字，目冠以（一）、（二）、（三）等數字，並應加具標點符號。"
 				},
 				{
-					content: "前項所定之目再細分者，冠以１、２、３等數字，並稱為第某目之１、２、３。",
+					"content": "前項所定之目再細分者，冠以１、２、３等數字，並稱為第某目之１、２、３。"
 				}
-			],
+			]
 		});
 	})
 	test('example2', () => {
@@ -47,29 +47,30 @@ describe('testtest', () => {
 		const res = parser.parse();
 		console.log(JSON.stringify(res));
 		expect(res).toEqual({
-			"stratum": 0,
+			"stratum": "paragraphs",
 			"list": [
 				{
-					"content": "法規有左列情形之一者，修正之："
-				}
-			],
-			"child": [
+					"content": "法規有左列情形之一者，修正之：",
+					"child": {
+						"stratum": "subparagraphs",
+						"list": [
+							{
+								"content": "一、基於政策或事實之需要，有增減內容之必要者。"
+							},
+							{
+								"content": "二、因有關法規之修正或廢止而應配合修正者。"
+							},
+							{
+								"content": "三、規定之主管機關或執行機關已裁併或變更者。"
+							},
+							{
+								"content": "四、同一事項規定於二以上之法規，無分別存在之必要者。"
+							}
+						]
+					}
+				},
 				{
-					"stratum": 1,
-					"list": [
-						{
-							"content": "一、基於政策或事實之需要，有增減內容之必要者。"
-						},
-						{
-							"content": "二、因有關法規之修正或廢止而應配合修正者。"
-						},
-						{
-							"content": "三、規定之主管機關或執行機關已裁併或變更者。"
-						},
-						{
-							"content": "四、同一事項規定於二以上之法規，無分別存在之必要者。"
-						}
-					]
+					"content": "法規修正之程序，準用本法有關法規制定之規定。"
 				}
 			]
 		});
@@ -87,26 +88,41 @@ describe('testtest', () => {
 		const res = parser.parse();
 		console.log(JSON.stringify(res));
 		expect(res).toEqual({
-			"stratum": 0,
+			"stratum": "paragraphs",
 			"list": [
 				{
-					"content": "委員會議議程依左列次序編列，於會前分送各出席及列席人員："
-				}
-			],
-			"child": [
-				{
-					"stratum": 1,
-					"list": [
-						{
-							"content": "一、報告事項："
-						},
-						{
-							"content": "二、討論事項。"
-						},
-						{
-							"content": "三、臨時動議。"
-						}
-					]
+					"content": "委員會議議程依左列次序編列，於會前分送各出席及列席人員：",
+					"child": {
+						"stratum": "subparagraphs",
+						"list": [
+							{
+								"content": "一、報告事項：",
+								"child": {
+									"stratum": "items",
+									"list": [
+										{
+											"content": "（一）上次委員會議議決案件執行情形報告。"
+										},
+										{
+											"content": "（二）本會重要措施。"
+										},
+										{
+											"content": "（三）專案研究或重要工作進度報告。"
+										},
+										{
+											"content": "（四）委員報告。"
+										}
+									]
+								}
+							},
+							{
+								"content": "二、討論事項。"
+							},
+							{
+								"content": "三、臨時動議。"
+							}
+						]
+					}
 				}
 			]
 		});
--- ts/leetcode-test/index.ts
+++ ts/leetcode-test/index.ts
@@ -1,112 +1,121 @@
 
 interface lawContent {
-  content: string
+	content: string
 }
 
 interface Law {
-  stratum?: Level,
-  list?: Law[],
-  child?: Law[]
-  content?: string,
+	stratum?: string,
+	list?: Law[],
+	child?: Law
+	content?: string,
 }
 
-
-
+const levelStrings = [
+	"paragraphs",
+	"subparagraphs",
+	"items"
+]
 enum Level {
-  paragraphs = 0,
-  subparagraphs = 1,
-  items = 2,
+	paragraphs = 0,
+	subparagraphs = 1,
+	items = 2,
 }
 
 class Parser {
-  private rawContent: string;
-  private lawContents: string[];
-  /**
-   * Index of current lawContent
-   */
-  private idx: number;
+	private rawContent: string;
+	private lawContents: string[];
+	/**
+	 * Index of current lawContent
+	 */
+	private idx: number;
 
-  constructor(content: string) {
-    this.rawContent = content;
-    this.lawContents = [];
-    this.idx = 0;
-  }
+	constructor(content: string) {
+		this.rawContent = content;
+		this.lawContents = [];
+		this.idx = 0;
+	}
 
-  parse(): Law {
-    this.lawContents = this.flatLawContent(this.rawContent);
-    return this.parseLaw(0);
-  }
+	parse(): Law {
+		this.lawContents = this.flatLawContent(this.rawContent);
+		return this.parseLaw(0);
+	}
 
-  private flatLawContent(rawData: string) {
-    return rawData.split("\n");
-  }
+	private flatLawContent(rawData: string) {
+		return rawData.split("\n");
+	}
 
-  private nextContent(): string | null {
-    if (this.idx >= this.lawContents.length) {
-      return null;
-    }
-    const content = this.lawContents[this.idx];
-    this.idx++;
-    return content;
-  }
+	private nextContent(): string | null {
+		if (this.idx >= this.lawContents.length) {
+			return null;
+		}
+		const content = this.lawContents[this.idx];
+		this.idx++;
+		return content;
+	}
 
-  getLevel(content: string): Level {
-    // 項:沒有數字
-    const regList = [
-      //  // 項:沒有數字
-      new RegExp(/^[^一二三四五六七八九十]/, 'g'),
-      // 「款冠以一、二、三等數字」：特徵必定為 一、、二、 ... 諸此類推，使用的標點符號必定為全行頓號 、。
-      new RegExp(/^(一|二|三|四|五|六|七|八|九|十)+、/, 'g'),
-      // 「目冠以（一）、（二）、（三）等數字」：特徵必定為 （一）、（二） ... 諸此類推，使用的標點符號必定為全形括號 （） 或半形括號 ()。
-      new RegExp(/^(\(（)(一二三四五六七八九十)+(\)）)/, 'g'),
-    ];
-    for (const [i, parser] of regList.entries()) {
-      if (parser.test(content)) {
-        return i;
-      }
-    }
-    throw new Error(`No matched RegExp at ${this.lawContents[this.idx]}`);
-  }
+	getLevel(content: string): Level {
+		// 項:沒有數字
+		const regList = [
+			//  // 項:沒有數字
+			// 廢棄
+			new RegExp(/^[^一二三四五六七八九十]/, 'g'),
+			// 「款冠以一、二、三等數字」：特徵必定為 一、、二、 ... 諸此類推，使用的標點符號必定為全行頓號 、。
+			new RegExp(/^[一二三四五六七八九十]+、/, 'g'),
+			// 「目冠以（一）、（二）、（三）等數字」：特徵必定為 （一）、（二） ... 諸此類推，使用的標點符號必定為全形括號 （） 或半形括號 ()。
+			new RegExp(/^[\(（][一二三四五六七八九十]+[\)）]/, 'g'),
+		];
+		for (const [i, parser] of regList.slice(1).entries()) {
+			if (parser.test(content)) {
+				return i + 1;
+			}
+		}
+		// nothing match: it's 項
+		return 0;
+		// throw new Error(`No matched RegExp at ${this.lawContents[this.idx]}`);
+	}
 
 
-  private parseLaw(level: Level): Law {
-    const curLaw: Law = {
-      stratum: level,
-      list: []
-    };
-    for (let content = this.nextContent(); content !== null; content = this.nextContent()) {
-      // e.g.
-      // [a], push a
-      // [a, b], push b
-      switch (this.getLevel(content)) {
-        case level: {
-          curLaw.list!.push({
-            // if has child, then we should set stratum 
-            content: content,
-          })
-          // same level
-          break;
-        }
-        case level + 1: {
-          curLaw.child = [];
-          this.stepBack();
-          curLaw.child.push(this.parseLaw(level + 1));
-          break;
-        }
-        case level - 1: {
-          // done, let caller keep parsing this.lawsContents
-          break;
-        }
-        default:
-          throw new Error(`Wrong Level: ${level}`);
-      }
-    }
-    return curLaw;
-  }
-  stepBack() {
-    if (this.idx > 0)
-      this.idx--;
-  }
+	private parseLaw(level: Level): Law {
+		const curLaw: Law = {
+			stratum: levelStrings[level],
+			list: []
+		};
+		for (let content = this.nextContent(); content !== null; content = this.nextContent()) {
+			// e.g.
+			// [a], push a
+			// [a, b], push b
+			console.log(`curLevel: ${level}, level of ${content}: ${this.getLevel(content)}`)
+			switch (this.getLevel(content)) {
+				case level: {
+					curLaw.list!.push({
+						// if has child, then we should set stratum 
+						content: content,
+					})
+					// same level
+					break;
+				}
+				case level + 1: {
+					this.stepBack();
+					const last = curLaw.list!.pop();
+					last!.child! = this.parseLaw(level + 1);
+					curLaw.list?.push(last!);
+					break;
+				}
+				case level - 1: {
+					// done, let caller keep parsing this.lawsContents
+					this.stepBack();
+					return curLaw;
+				}
+				default:
+					throw new Error(`Wrong Level: ${level}`);
+			}
+		}
+		return curLaw;
+	}
+	stepBack() {
+		if (this.idx > 0)
+			this.idx--;
+	}
 }
 
 
