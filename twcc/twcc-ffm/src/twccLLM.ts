e11560120347a2c4cbdbad3c59eb5e459e860463
unknowntpo <e850506@gmail.com>
2024-05-20 15:06:32 +0800

    refactor(twcc-ffm): import

--- twcc/twcc-ffm/src/twccLLM.ts
+++ twcc/twcc-ffm/src/twccLLM.ts
@@ -1,10 +1,32 @@
-import { ChatResponse, ChatResponseChunk, CompletionResponse, LLM, LLMChatParamsNonStreaming, LLMChatParamsStreaming, LLMCompletionParamsNonStreaming, LLMCompletionParamsStreaming, LLMMetadata } from 'llamaindex';
+import {
+	ChatResponse,
+	ChatResponseChunk,
+	CompletionResponse,
+	LLM,
+	LLMChatParamsNonStreaming,
+	LLMChatParamsStreaming,
+	LLMCompletionParamsNonStreaming,
+	LLMCompletionParamsStreaming,
+	LLMMetadata,
+	Tokenizers
+} from 'llamaindex';
 
 function sum(x: number, y: number): number { return x + y };
 
 
 class TwccLLM implements LLM {
 	metadata: LLMMetadata;
+	constructor() {
+		this.metadata = {
+			model: 'abc',
+			temperature: 0,
+			topP: 1,
+			maxTokens: 350,
+			contextWindow: 1000,
+			tokenizer: undefined,
+		}
+	}
+
 	chat(params: LLMChatParamsStreaming<object, object>): Promise<AsyncIterable<ChatResponseChunk<object>>>;
 	chat(params: LLMChatParamsNonStreaming<object, object>): Promise<ChatResponse<object>>;
 	chat(params: unknown): Promise<AsyncIterable<ChatResponseChunk<object>>> | Promise<ChatResponse<object>> {
