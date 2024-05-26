5633e5aa3748c48c3851496e545bf1df86b59b27
unknowntpo <e850506@gmail.com>
2024-05-26 10:00:54 +0800

    feat(nats/nats-example): add jestream

--- nats/nats-example/src/index.ts
+++ nats/nats-example/src/index.ts
@@ -1,69 +1,137 @@
-import { NatsConnection, StringCodec, Subscription, connect } from "nats";
+import {
+  NatsConnection, RetentionPolicy, AckPolicy, StringCodec, Subscription, connect
+} from "nats";
 const server = { servers: "localhost:4222" };
 
 async function pubSub() {
-	const nc = await connect(server);
-	console.log(`connected to ${nc.getServer()}`);
+  const nc = await connect(server);
+  console.log(`connected to ${nc.getServer()}`);
 
 
-	// Create three subscribers
-	const sub1 = createSubscriber(nc, 1);
-	const sub2 = createSubscriber(nc, 2);
-	const sub3 = createSubscriber(nc, 3);
-	// Publish messages
-	publishMessages(nc);
+  // Create three subscribers
+  const sub1 = createSubscriber(nc, 1);
+  const sub2 = createSubscriber(nc, 2);
+  const sub3 = createSubscriber(nc, 3);
+  // Publish messages
+  publishMessages(nc);
 
-	// Handle graceful shutdown
-	const shutdown = async () => {
-		console.log("Shutting down...");
+  // Handle graceful shutdown
+  const shutdown = async () => {
+    console.log("Shutting down...");
 
-		// Close the subscribers
-		sub1.unsubscribe();
-		sub2.unsubscribe();
-		sub3.unsubscribe();
+    // Close the subscribers
+    sub1.unsubscribe();
+    sub2.unsubscribe();
+    sub3.unsubscribe();
 
-		// Close the connection
-		await nc.close();
-		console.log(`Connection closed`);
+    // Close the connection
+    await nc.close();
+    console.log(`Connection closed`);
 
-		// Check if the close was OK
-		const err = await nc.closed();
-		if (err) {
-			console.log(`Error closing:`, err);
-		}
+    // Check if the close was OK
+    const err = await nc.closed();
+    if (err) {
+      console.log(`Error closing:`, err);
+    }
 
-		process.exit();
-	};
+    process.exit();
+  };
 
-	process.on("SIGINT", shutdown);
-	process.on("SIGTERM", shutdown);
+  process.on("SIGINT", shutdown);
+  process.on("SIGTERM", shutdown);
 }
 
 const sc = StringCodec();
 
 // Create a publisher
 function publishMessages(nc: NatsConnection) {
-	const messages = ["Hello, World!", "NATS is awesome!", "Pub-Sub example"];
-	let messageIndex = 0;
+  const messages = ["Hello, World!", "NATS is awesome!", "Pub-Sub example"];
+  let messageIndex = 0;
 
-	// Publish a message every second
-	setInterval(() => {
-		const msg = messages[messageIndex];
-		nc.publish("test.subject", sc.encode(msg));
-		console.log(`Publisher: Published message ${messageIndex + 1}: ${msg}`);
-		messageIndex = (messageIndex + 1) % messages.length;
-	}, 1000);
+  // Publish a message every second
+  setInterval(() => {
+    const msg = messages[messageIndex];
+    nc.publish("test.subject", sc.encode(msg));
+    console.log(`Publisher: Published message ${messageIndex + 1}: ${msg}`);
+    messageIndex = (messageIndex + 1) % messages.length;
+  }, 1000);
 }
 
 // Create a subscriber
 function createSubscriber(nc: NatsConnection, id: number): Subscription {
-	const sub = nc.subscribe("test.subject");
-	(async () => {
-		for await (const msg of sub) {
-			console.log(`Subscriber ${id}: Received message: ${sc.decode(msg.data)}`);
-		}
-	})();
-	return sub;
+  const sub = nc.subscribe("test.subject");
+  (async () => {
+    for await (const msg of sub) {
+      console.log(`Subscriber ${id}: Received message: ${sc.decode(msg.data)}`);
+    }
+  })();
+  return sub;
 }
 
-pubSub()
+async function jetStream() {
+  const nc = await connect(server);
+  console.log(`connected to ${nc.getServer()}`);
+
+
+  const js = nc.jetstream();
+
+  const jsm = await js.jetstreamManager();
+  await jsm.streams.add({
+    name: "EVENTS",
+    retention: RetentionPolicy.Workqueue,
+    subjects: ["events.>"],
+  })
+  console.log(`created the stream`)
+
+  await Promise.all([
+    js.publish("events.us.page_loaded"),
+    js.publish("events.us.mouse_clicked"),
+    js.publish("events.us.input_focused"),
+  ])
+
+  console.log("# Stream info without consumers");
+  console.log((await jsm.streams.info("EVENTS")).state);
+
+
+  await jsm.consumers.add("EVENTS", {
+    durable_name: "worker",
+    ack_policy: AckPolicy.Explicit,
+  });
+
+  const c = await js.consumers.get("EVENTS", "worker");
+
+  const iter = await c.fetch({ max_messages: 3 });
+  for await (const m of iter) {
+    console.log(`got: ${m.subject}`);
+    m.ack();
+  }
+
+  console.log("# Stream info with one consumer");
+  console.log((await jsm.streams.info("EVENTS")).state);
+
+
+
+  // Handle graceful shutdown
+  const shutdown = async () => {
+    console.log("Shutting down...");
+
+    // Close the connection
+    await nc.close();
+    console.log(`Connection closed`);
+
+    // Check if the close was OK
+    const err = await nc.closed();
+    if (err) {
+      console.log(`Error closing:`, err);
+    }
+
+    process.exit();
+  };
+
+  process.on("SIGINT", shutdown);
+  process.on("SIGTERM", shutdown);
+}
+
+jetStream()
+
+// pubSub()
