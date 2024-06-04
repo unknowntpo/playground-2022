b7790f0007d8bb262a9c1a1c90a3e4843a422945
unknowntpo <e850506@gmail.com>
2024-06-04 08:39:19 +0800

    feat(java:mockito-example): add verify call times

--- java/mockito-example/app/src/test/java/org/example/PersonTest.java
+++ java/mockito-example/app/src/test/java/org/example/PersonTest.java
@@ -1,11 +1,9 @@
 package org.example;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 public class PersonTest {
     @Test
@@ -15,5 +13,16 @@ public class PersonTest {
         Person person = new Person(mockCalculator);
 
         assertEquals(4, person.add(2,3));
+    }
+    @Test
+    public void testAddThreeTimes() {
+        Calculator mockCalculator = mock(Calculator.class);
+        Person person = new Person(mockCalculator);
+        final int CALL_TIMES = 3;
+        for (int i = 0 ; i < CALL_TIMES ; i++) {
+            person.add(2,3);
+        }
+        // assert add is called three times
+        verify(mockCalculator, times(CALL_TIMES)).add(2,3);
     }
 }
