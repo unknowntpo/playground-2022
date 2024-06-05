50613cdcff617e6c265fd7c05e63d2986618dcfc
unknowntpo <e850506@gmail.com>
2024-06-05 08:48:15 +0800

    feat(duckdb-example:duckdb-async): add failed example

--- js/duckdb-example/src/index.js
+++ js/duckdb-example/src/index.js
@@ -18,22 +18,20 @@ async function simpleTest() {
   console.log(sumN);
 }
 
-// simpleTest();
-
 async function fetchFromHttp() {
   const db = await Database.create(":memory:");
 
-  db.register_udf('css_selector', 'string', (url) => {
+  db.register_udf('css_selector', 'string', async (url) => {
     console.log(`fetching from url: ${url}`);
     const data = fetch(url)
       .then(res => res.text());
-    return data;
+    return await data;
   });
 
   const rows = await db.all(`select css_selector('https://example.com/') as html`);
   await db.wait();
 
-  console.log(rows);
+  console.log(JSON.stringify(rows, null, '\t'));
 }
 
 await fetchFromHttp();
