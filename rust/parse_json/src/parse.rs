9793d9fcaae831326bb99478e5868cfbad05cbd2
unknowntpo <e850506@gmail.com>
2024-06-26 07:07:38 +0800

    feat(rs:parse_json): add validator example

--- rust/parse_json/src/parse.rs
+++ rust/parse_json/src/parse.rs
@@ -1,9 +1,13 @@
-use serde::{Deserialize, Serialize};
+use std::borrow::Cow;
 
-#[derive(Debug, Serialize, Deserialize)]
+use serde::{Deserialize, Serialize};
+use validator::{Validate, ValidationError};
+
+#[derive(Debug, Serialize, Deserialize, Validate)]
 struct Employee {
     id: String,
     employee_name: String,
+    #[validate(custom(function=validate_employee_salary))]
     employee_salary: String,
     employee_age: String,
     profile_image: String,
@@ -16,27 +20,27 @@ struct Resp {
 }
 
 pub fn parse(json_data: &str) {
-    let json_data = r#"
-    {
-        "status": "success",
-        "data": [
-            {
-                "id": "1",
-                "employee_name": "Tiger Nixon",
-                "employee_salary": "320800",
-                "employee_age": "61",
-                "profile_image": ""
-            }
-        ]
-    }
-    "#;
     match serde_json::from_str::<Resp>(json_data) {
         Ok(r) => {
             println!("Status: {}", r.status);
             for employee in r.data {
                 println!("Employee: {:?}", employee);
+                match employee.validate() {
+                    Ok(_) => println!("validation PASS"),
+                    Err(e) => println!("validation failed: {}", e),
+                };
             }
         }
         Err(e) => println!("Got error: {}", e),
     };
+}
+
+fn validate_employee_salary(salary: &str) -> Result<(), ValidationError> {
+    let salary: f64 = salary.parse().map_err(|_| ValidationError::new("failed"))?;
+
+    if salary > 3000.0 {
+        Err(ValidationError::new("Salary exceeds maximum limit"))
+    } else {
+        Ok(())
+    }
 }
