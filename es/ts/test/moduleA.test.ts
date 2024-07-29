eec54832cfab0563d03a10272427fb74171fade8
unknowntpo <e850506@gmail.com>
2024-07-29 14:42:07 +0800

    feat(es): add example

---
+++ es/ts/test/moduleA.test.ts
@@ -0,0 +1,9 @@
+import { describe, test, expect } from 'vitest'
+
+import { abc } from "src/moduleA";
+
+describe("project tests", () => {
+	test("moduleA is correct", async () => {
+		expect(abc).toEqual("123");
+	});
+});
