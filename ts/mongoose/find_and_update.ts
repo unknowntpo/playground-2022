dd1bd1b4783c683f8f106176f9bfc628cd7a2f63
unknowntpo <e850506@gmail.com>
2024-08-06 10:35:08 +0800

    feat(ts/mongoose): test: add validator

--- ts/mongoose/find_and_update.ts
+++ ts/mongoose/find_and_update.ts
@@ -1,15 +1,25 @@
 import mongoose, { ValidatorProps } from "mongoose";
 
+const titleValidator = function (title: string): boolean {
+	return title.length > 4
+}
+
 const blogSchema = new mongoose.Schema<blog>({
-	title: String, // String is shorthand for {type: String}
+	title: {
+		type: String, // String is shorthand for {type: String}
+		validate: {
+			validator: (v: string) => v.length > 3,
+			message: (props) => `${props.value} is not valid!`,
+		},
+	},
 	author: String,
 	body: String,
 }, { timestamps: true });
 
 interface blog {
-	title: String, // String is shorthand for {type: String}
-	author: String,
-	body: String,
+	title: string,
+	author: string,
+	body: string,
 }
 
 const blogModel = mongoose.model<blog>('Blog', blogSchema);
@@ -41,7 +51,12 @@ const main = async () => {
 	assertNotNull(updatedBlog)
 	console.log(`updatedBlog: ${JSON.stringify(updatedBlog, null, "\t")}`)
 
-
+	// update partial doc, not including createdAt, updatedAt
+	const updatedFailedBlog: blog | null = await blogModel.findOneAndUpdate({ _id: blog._id }, { "title": "aa", createdAt: new Date("1900-01-06T01:01:01.000Z"), }, {
+		runValidators: true
+	}).lean();
+	assertNotNull(updatedBlog)
+	console.log(`updatedBlog: ${JSON.stringify(updatedFailedBlog, null, "\t")}`)
 
 	// replace ->replace ful doc, including createdAt, updatedAt
 	const replacedBlog: blog | null = await blogModel.findOneAndReplace({ _id: blog._id }, { "title": "second blog", createdAt: new Date("1900-01-06T01:01:01.000Z"), }).lean();
