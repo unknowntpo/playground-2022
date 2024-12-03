1b5d7ded5c1ff2f13d0f0558028ef873cdcf8c49
Eric Chang <e850506@gmail.com>
2024-12-03 13:39:39 +0800

    feat(rust/github_api): decode json and get commiter name

--- rust/github_api/src/most_active_contributor.rs
+++ rust/github_api/src/most_active_contributor.rs
@@ -5,19 +5,41 @@ use serde::Deserialize;
 struct Commit {
     sha: String,
     node_id: String,
-    commit: Option<String>,
-    url: String,
-    html_url: u32,
-    comments_url: u32,
-    author: String,
+    commit: CommitInfo,
+}
+
+#[derive(Deserialize, Debug)]
+struct CommitInfo {
+    author: Author,
 }
 
 #[derive(Deserialize, Debug)]
 struct Author {
-    login: String,
+    name: String,
+    email: String,
+    date: String,
 }
 
 pub async fn do_work() -> Result<()> {
     println!("Helo");
+
+    let url = "https://api.github.com/repos/apache/gravitino/commits";
+
+    let client = reqwest::Client::new();
+    let response = client
+        .get(url)
+        .header("User-Agent", "rust-github-api-demo")
+        .send()
+        .await?;
+
+    if response.status().is_success() {
+        // FIXME: error handling
+        let commits: Vec<Commit> = response.json().await?;
+
+        for c in commits {
+            println!("Commit: {:?}", c.commit.author.name);
+        }
+    }
+
     Ok(())
 }
