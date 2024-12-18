1ca49d5b8891bb42c6e62f1edf35ed0465aca746
Eric Chang <e850506@gmail.com>
2024-12-18 10:37:42 +0800

    feat(rust/tracing_subscriber_example): use instrument to do debug logging

--- rust/tracing_subscriber_example/src/main.rs
+++ rust/tracing_subscriber_example/src/main.rs
@@ -1,5 +1,12 @@
 use log::{debug, info, trace};
 
+use tracing::instrument;
+
+#[instrument(level = "debug", skip(b), ret, err)]
+fn custom_fn(a: i32, b: i32, c: i32) -> Result<i32, String> {
+    Ok(3)
+}
+
 fn main() {
     tracing_subscriber::fmt::init();
 
@@ -9,6 +16,9 @@ fn main() {
     debug!("this is debug log: {}", 4);
 
     trace!("this is trace log: {}", 4);
+
+    info!("{:?}", custom_fn(3, 1, 2));
+    info!("{:?}", custom_fn(4, 4, 1));
 }
 
 #[cfg(test)]
