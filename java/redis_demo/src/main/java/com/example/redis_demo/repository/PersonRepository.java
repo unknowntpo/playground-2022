4e138e99296c260f270bbaa20823811733fe37f1
Eric Chang <e850506@gmail.com>
2024-12-20 14:50:14 +0800

    feat(java/redis_demo): wip: spring data redis

---
+++ java/redis_demo/src/main/java/com/example/redis_demo/repository/PersonRepository.java
@@ -0,0 +1,9 @@
+package com.example.redis_demo.repository;
+
+import com.example.redis_demo.model.Person;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonRepository extends CrudRepository<Person, String> {
+}
