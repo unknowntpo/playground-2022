e44b1c0618668b32756cd25cadd4702736dbbec9
Eric Chang <e850506@gmail.com>
2024-12-23 17:08:09 +0800

    feat(java/spring/spring-ioc): use PostConstruct

--- java/spring/spring-ioc/app/src/main/java/org/example/controllers/HpPrinter.java
+++ java/spring/spring-ioc/app/src/main/java/org/example/controllers/HpPrinter.java
@@ -1,12 +1,19 @@
 package org.example.controllers;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Component;
 
 @Component
 public class HpPrinter implements Printer {
+    private int count;
+
+    @PostConstruct
+    void initialize() {
+        count = 5;
+    }
 
     @Override
     public void print(String message) {
-        System.out.printf("print from %s: %s\n", HpPrinter.class.toString(), message);
+        System.out.printf("print from %s: %s, count: %d\n", HpPrinter.class.toString(), message, count);
     }
 }
--- java/spring/spring-ioc/app/src/main/java/org/example/controllers/MyController.java
+++ java/spring/spring-ioc/app/src/main/java/org/example/controllers/MyController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MyController {
     @Autowired
-    @Qualifier("canonPrinter")
+    @Qualifier("hpPrinter")
     private Printer printer;
 
     @RequestMapping("/test")
