c5c0d7e2c4125226200b0d4f9f372ead6ae7edce
Eric Chang <e850506@gmail.com>
2024-12-27 10:54:16 +0800

    feat(java/spring/spring-mvc): create student table

--- java/spring/spring-mvc/app/src/main/java/org/example/entities/Student.java
+++ java/spring/spring-mvc/app/src/main/java/org/example/entities/Student.java
@@ -1,9 +1,20 @@
 package org.example.entities;
 
-public class Student {
-    private Integer id;
-    private String name;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "student")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(nullable = false, length = 100)
+    private String name;
 
     public Integer getId() {
         return id;
