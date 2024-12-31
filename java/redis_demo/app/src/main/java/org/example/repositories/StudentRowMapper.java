9ad8ad6e714855157aaf5c2499d4e7e52abd184f
Eric Chang <e850506@gmail.com>
2024-12-31 15:12:02 +0800

    feat(java/redis_demo): copied from spring/spring-mvc

---
+++ java/redis_demo/app/src/main/java/org/example/repositories/StudentRowMapper.java
@@ -0,0 +1,19 @@
+package org.example.repositories;
+
+import org.example.entities.Student;
+import org.springframework.jdbc.core.RowCallbackHandler;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class StudentRowMapper implements RowMapper<Student> {
+    @Override
+    public Student mapRow(ResultSet rs, int rowNum) throws SQLException {
+        Student student = new Student();
+        student.setId(rs.getInt("id"));
+        student.setName(rs.getString("name"));
+
+        return student;
+    }
+}
--- java/redis_demo/src/main/java/com/example/redis_demo/repository/PersonRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.redis_demo.repository;
-
-import com.example.redis_demo.model.Person;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PersonRepository extends CrudRepository<Person, String> {
-}
