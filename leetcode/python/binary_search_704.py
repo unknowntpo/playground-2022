7d58e59fd3fa87d5d07230f31b3c5d59fe3db740
Eric Chang <e850506@gmail.com>
2025-01-09 11:29:13 +0800

    feat(leetcode/python/binary_search_704): add [left,right), test failed approach (left,right]

--- leetcode/python/binary_search_704.py
+++ leetcode/python/binary_search_704.py
@@ -27,6 +27,8 @@ Example:
 [3], target: 1, res: -1
 
 """
+
+
 class Solution:
     # [left, right] (closed interval)
     def search(self, nums: List[int], target: int) -> int:
@@ -36,22 +38,84 @@ class Solution:
             # FIXME: may overflow
             # m = int(l / 2 + r / 2)
             # FIXME why equals
-           m = l + (r - l) // 2
-           midNum = nums[m]
-           if midNum == target:
-               return m
-           if target > midNum:
-               l = m
-           else:
+            m = l + (r - l) // 2
+            midNum = nums[m]
+            if midNum == target:
+                return m
+            if target > midNum:
+                l = m
+            else:
                 r = m
 
         return -1
+
+    # [left,right)
+    # e.g. [0, 1)
+    # [-1, 0, 3, 5, 9, 12], target: 9, want: 4
+    def search_left_close_right_open(self, nums: List[int], target: int) -> int:
+        l = 0
+        r = len(nums)
+        while l < r:
+            m = l + (r - l) // 2
+            mVal = nums[m]
+            if mVal == target:
+                return m
+            if mVal < target:
+                l = m + 1
+            else:
+                r = m
+        return -1
+
+    # (left,right]
+    # e.g. (0, 1]
+    # [-1, 0, 3, 5, 9, 12], target: 9, want: 4
+    # [0], target: 3
+    # [3], target: 1
+    # [-1, 0, 3, 5, 9, 12], target: 2, want: -1
+    # [1], target: 1
+    """
+    this will failed, 
+    testcase
+    # [1], target: 1
+    becausea m will be out-of-bound (-1 + 0) // 2 = -1
+    which is invalid.
+    breaks invariant
+    l < 0 <= m <= right < len(nums) - 1 
+    """
+    def search_left_open_right_close(self, nums: List[int], target: int) -> int:
+        l = -1
+        r = len(nums) - 1
+        # l = -1, r = 5
+        # l = -1, r = 0
+        while l < r:
+            m = l + (r - l) // 2
+            # m = 4 / 2 = 2
+            # m = -1 + 0 // 2 = -1
+            # m = (1 + (-1)) // 2 = 0
+            # m = 0
+            # l = -1, r = 0, m = -1, out of bound
+            if m < 0 or m > len(nums) - 1:
+                return -1
+            mVal = nums[m]
+            # mVal = 3
+            # mVal = -1
+            if mVal == target:
+                return m
+            if mVal < target:
+                l = m + 1
+            else:
+                r = m
+                # r = 2 - 1 = 1, l = -1
+                # r = 0 - 1 = -1, l = -1
+        return -1
+
 
     def search_slow(self, nums: List[int], target: int) -> int:
         for i, num in enumerate(nums):
             if num == target:
                 return i
         return -1
+
 
 testCases = [
     {"name": "found", "nums": [-1, 0, 3, 5, 9, 12], "target": 9, "want": 4},
@@ -60,10 +124,17 @@ testCases = [
     {"name": "only 1 number - not found", "nums": [3], "target": 1, "want": -1},
 ]
 
+
 @pytest.mark.parametrize("testCase", testCases, ids=lambda testCase: testCase["name"])
 def test_binary_search(testCase):
     nums = testCase["nums"]
     target = testCase["target"]
     sol = Solution()
-    res = sol.search(nums, target)
-    assert res == testCase["want"]
+
+
+    method_names = ['search', 'search_slow', 'search_left_close_right_open']
+    fns = [getattr(sol, method_name) for method_name in method_names]
+    for f in fns:
+        print(f"testing {f.__name__}...")
+        res = f(nums, target)
+        assert res == testCase["want"]
