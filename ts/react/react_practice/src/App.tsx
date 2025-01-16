16901d227832e8129f4da0f01713fe54045b3f1e
Eric Chang <e850506@gmail.com>
2025-01-16 10:08:20 +0800

    refactor(ts/react/react_practice): decrementCountTwice

--- ts/react/react_practice/src/App.tsx
+++ ts/react/react_practice/src/App.tsx
@@ -4,7 +4,12 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(10)
+
+	const decrementCountTwice = () => {
+		setCount((count) => count - 1);
+		setCount((count) => count - 1);
+	}
 
   return (
     <>
@@ -18,7 +23,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={decrementCountTwice}>
           count is {count}
         </button>
         <p>
