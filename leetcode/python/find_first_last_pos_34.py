66cd54e93261db1c25c58b03cc72a625fcec44d3
Eric Chang <e850506@gmail.com>
2025-01-19 11:12:44 +0800

    feat(leetcode/python/find_first_last_pos_34): make it pass

---
+++ leetcode/python/find_first_last_pos_34.py
@@ -0,0 +1,166 @@
+from collections import deque
+from ctypes.wintypes import tagMSG
+from os import times_result
+
+import pytest
+from typing import List, Optional, Deque
+
+from binary_tree.tree import BinaryTree
+
+"""
+给你一个按照非递减顺序排列的整数数组 nums，和一个目标值 target。请你找出给定目标值在数组中的开始位置和结束位置。
+
+如果数组中不存在目标值 target，返回 [-1, -1]。
+
+你必须设计并实现时间复杂度为 O(log n) 的算法解决此问题。
+
+示例 1：
+
+输入：nums = [5,7,7,8,8,10], target = 8
+输出：[3,4]
+示例 2：
+
+输入：nums = [5,7,7,8,8,10], target = 6
+输出：[-1,-1]
+示例 3：
+
+输入：nums = [], target = 0
+输出：[-1,-1]
+ 
+
+提示：
+
+0 <= nums.length <= 105
+-109 <= nums[i] <= 109
+nums 是一个非递减数组
+-109 <= target <= 109
+"""
+
+
+class Solution:
+    """
+    输入：nums = [5,7,7,8,8,10], target = 8
+    输出：[3,4]
+    示例 2：
+    """
+
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        """
+        [5,7,7,8,8,10]
+
+        lower_bound(target), lower_bound(target+1)
+
+        lower_bound(n): find the first element which nums[ele] >= n
+
+        target <= nums[lower_bound]
+
+        target+1 <= nums[lower_bound2]
+
+        target < nums[lower_bound2]
+
+        return [lower_bound, lower_bound2]
+
+        lower_bound: found: got a num
+        lower_bound not found: loop invariant will break
+
+        [5,7,7,8,8,10]
+
+        close interval
+        [left,right]
+
+        ""
+        Example
+        [5,7,7,8,8,10]
+
+        [1], target = 2
+        [1], target = 0
+
+        [1], target = 2
+        l = 0, r = 0
+        mid = 0
+        target > mid
+        l = 1
+
+        [1], target = 0
+        l = 0, r = 0
+        mid = 0
+        target < mid
+        r = 0 - 1 = -1
+
+        [5,7,7,8,8,10]
+        target = 9
+        l = 0, r = 5
+        mid = 2
+        nums[target] > nums[mid]
+        l = mid + 1 = 3, r = 5
+
+        mid = 3 + (5-3) // 2 = 4
+        nums[target] > mid
+        l = mid + 1 = 4 + 1 = 5
+        post
+        0 <= l <= r
+        (not match) <= l
+        matched >= r
+
+        --
+
+
+        """
+        def lower_bound(nums: List[int], n: int):
+            print(n)
+            # invariant: [l,r] close interval
+            # in interval: values needs to be search
+
+            # [5,7,7,8,8,10]
+            # target: 9
+            l = 0
+            r = len(nums) - 1
+            # l = 0, r = 5
+            while l <= r:
+               mid = l + (r - l) // 2
+                # mid = 0 + ( 5 - 0 ) // 2 = 2
+               # mid = 3 + (5-3) // 2 = 4
+               # mid = 5
+               midVal = nums[mid]
+               if n > midVal:
+                    # 8 > 7
+                    # 10 > 8
+                    l = mid + 1
+                    # blue: [mid+1, r]
+                    # l = 2 + 1 = 3, r = 5
+                    # l = 4 + 1 = 5, r = 5
+               else:
+                    r = mid - 1
+            print(l)
+            return l
+
+        lower = lower_bound(nums, target)
+        if lower == len(nums) or nums[lower] != target:
+            return [-1, -1]
+
+        # at here, target must in nums
+        upper = lower_bound(nums, target + 1) - 1
+
+        return [lower, upper]
+
+testCases = [
+    {"name": "found", "nums": [5, 7, 7, 8, 8, 10], "target": 8, "want": [3, 4]},
+    {"name": "not found", "nums": [5, 7, 7, 8, 8, 10], "target": 6, "want": [-1, -1]},
+    {"name": "empty", "nums": [], "target": 0, "want": [-1, -1]},
+    {"name": "only 1 number - found", "nums": [1], "target": 1, "want": [0, 0]},
+    {"name": "only 1 number - not found", "nums": [1], "target": 3, "want": [-1, -1]},
+]
+
+
+@pytest.mark.parametrize("testCase", testCases, ids=lambda testCase: testCase["name"])
+def test_find_first_last_pos(testCase):
+    nums = testCase["nums"]
+    target = testCase["target"]
+    sol = Solution()
+
+    method_names = ['searchRange']
+    fns = [getattr(sol, method_name) for method_name in method_names]
+    for f in fns:
+        print(f"testing {f.__name__}...")
+        res = f(nums, target)
+        assert res == testCase["want"]
