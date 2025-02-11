0a80aa4d8b803ef6dac71a54bca5a710570e67b5
Eric Chang <e850506@gmail.com>
2025-02-11 09:32:04 +0800

    feat(ts/react/react_practice): dynamically changing css

--- ts/react/react_practice/src/App.tsx
+++ ts/react/react_practice/src/App.tsx
@@ -2,14 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import ColorButton from './components/ColorButton'
 
 function App() {
   const [count, setCount] = useState(10)
 
-	const decrementCountTwice = () => {
-		setCount((count) => count - 1);
+	const decrementCount = () => {
 		setCount((count) => count - 1);
 	}
+
+	const incrementCount= () => {
+		setCount((count) => count + 1);
+	}
+
 
   return (
     <>
@@ -23,10 +28,17 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={decrementCountTwice}>
-          count is {count}
+				<button onClick={incrementCount}>
+					+
         </button>
-        <p>
+				<ColorButton />
+        <p className="count">
+					Count is {count}
+        </p>
+        <button onClick={decrementCount}>
+					-
+        </button>
+				<p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
