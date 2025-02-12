60c18e0b600077a9ad961b35cd1d74c8b5a7ab04
Eric Chang <e850506@gmail.com>
2025-02-12 10:41:51 +0800

    feat(ts/react/login-signup): add useState hook

--- ts/react/login_signup/src/components/LoginSignup.tsx
+++ ts/react/login_signup/src/components/LoginSignup.tsx
@@ -2,13 +2,15 @@ import user_icon from '../assets/person.png'
 import password_icon from '../assets/password.png'
 import email_icon from '../assets/email.png'
 import './LoginSignup.css'
+import { useState } from 'react'
 
 
 const LoginSignup = () => {
+	const [action, setAction] = useState("Login");
 	return <div>
 		<div className='container'>
 			<div className='header'>
-				<div className='text'>Sign up</div>
+				<div className='text'>{action}</div>
 				<div className='underline'></div>
 			</div>
 			<div className="inputs">
@@ -27,8 +29,8 @@ const LoginSignup = () => {
 			</div>
 			<div className="forgot-password">Forgot Password? <span>Click Here</span></div>
 			<div className="submit-container">
-				<div className="submit">Sign up</div>
-				<div className="submit">Login</div>
+				<div className={ action === "Signup" ? "submit grey" : "submit" } onClick={() => setAction("Signup")}>Sign up</div>
+				<div className={ action === "Login" ? "submit grey" : "submit" } onClick={() => setAction("Login")}>Login</div>
 			</div>
 		</div>
 	</div>
