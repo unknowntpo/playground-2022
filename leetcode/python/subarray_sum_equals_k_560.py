3d4777c2b05b7357d1ec54ba995050df98300c30
Eric Chang <e850506@gmail.com>
2025-03-03 20:52:20 +0800

    refactor(leetcode/python/subarray_sum_equals_k_560.py): add subarraySumOn2PrefixSum 2 pass

--- leetcode/python/subarray_sum_equals_k_560.py
+++ leetcode/python/subarray_sum_equals_k_560.py
@@ -34,21 +34,27 @@ class Solution:
         return ans
 
     def subarraySumOn2PrefixSum(self, nums: List[int], k: int) -> int:
+        """
+        nums: [1, -1, 0], k = 0
+        ps: [0, 1, 0, 0]
+
+        nums: [1, 2, 3], k = 3, 'want': 2
+        ps: [0, 1, 3, 5]
+
+
+        """
         ans = 0
         n = len(nums)
         ps = [0] * (n + 1)
         for i in range(0, n):
             ps[i + 1] = ps[i] + nums[i]
 
-        for i in range(n):
-            total = 0
-            # find matching array in [i, j]
-            for j in range(i, n):
-                # total from [i, j]
-                total = ps[j + 1] - ps[i]
-
-                if total == k:
+        for r in range(0, len(ps)):
+            for l in range(0, r):
+                if ps[r] - ps[l] == k:
+                    print(f"ps[{r}] - ps[{l}] == {k}")
                     ans += 1
+
         return ans
 
     def subarraySum(self, nums: List[int], k: int) -> int:
@@ -65,6 +71,32 @@ class Solution:
         ans = 0
         cnt = defaultdict(int)
         # cnt = {}
+        """
+        [1,2,1], cnt: 
+
+        ps
+        [0, 1, 3, 4]
+
+        cnt:
+        {0: 1}
+        {0: 1, 1: 1}
+        {0: 1, 1: 1, 3: 1, 4: 1}
+
+        [0, 1, -1, 1]
+        ps:
+        [0, 0, 1, 0, 1]
+
+        cnt:
+        {0: 1} 0: ps0
+        {0: 1+1, } ps0, ps1
+        {0: 1+1, 1:1}, 0: ps0, ps1, 1: ps2
+        {0: 1+1, 1:1+1} 0: ps0, ps1, 1: ps2, ps3
+        {0: 1+1, 1:1+1, 2: 1} 0: ps0, ps1, 1: ps2, ps3, 2: ps4
+
+        k = 0
+
+        cnt: [0, i+1) prefix sum 
+        """
         for sj in s:
             # sj: s[0], s[1] = 1, s[2] = 2, s[3] = 3
             # si = sj -k
