e4fabb99ba326ed5e71c544de8932c7ae5166f2e
Eric Chang <e850506@gmail.com>
2025-03-22 07:48:02 +0800

    feat(leetcode/rs/subsets_78.rs): add two approach

--- leetcode/rs/src/subsets_78.rs
+++ leetcode/rs/src/subsets_78.rs
@@ -4,8 +4,50 @@ struct Solution;
 
 impl Solution {
     pub fn subsets(nums: Vec<i32>) -> Vec<Vec<i32>> {
-        // let res = vec![];
-        return vec![];
+        let n = nums.len() as i32;
+
+        let mut res: Vec<Vec<i32>> = vec![];
+        let mut path: Vec<i32> = vec![];
+
+        fn dfs(i: i32, n: i32, nums: &Vec<i32>, path: &mut Vec<i32>, res: &mut Vec<Vec<i32>>) {
+            if i == n {
+                res.push(path[..].to_vec());
+                return;
+            }
+
+            path.push(nums[i as usize]);
+            dfs(i + 1, n, nums, path, res);
+            path.pop();
+            dfs(i + 1, n, nums, path, res);
+        };
+
+        dfs(0, n, &nums, &mut path, &mut res);
+
+        return res;
+    }
+
+    pub fn subsets2(nums: Vec<i32>) -> Vec<Vec<i32>> {
+        let n = nums.len() as i32;
+
+        let mut res: Vec<Vec<i32>> = vec![];
+        let mut path: Vec<i32> = vec![];
+
+        fn dfs(i: i32, n: i32, nums: &Vec<i32>, path: &mut Vec<i32>, res: &mut Vec<Vec<i32>>) {
+            res.push(path[..].to_vec());
+            if i == n {
+                return;
+            }
+
+            for i in i..n {
+                path.push(nums[i as usize]);
+                dfs(i + 1, n, nums, path, res);
+                path.pop();
+            }
+        };
+
+        dfs(0, n, &nums, &mut path, &mut res);
+
+        return res;
     }
 }
 
@@ -25,7 +67,8 @@ mod tests {
         vec![1, 2, 3],
     ])]
     fn test_subsets(#[case] nums: Vec<i32>, #[case] want: Vec<Vec<i32>>) {
-        let mut got = Solution::subsets(nums);
+        // let mut got = Solution::subsets(nums);
+        let mut got = Solution::subsets2(nums);
         got.sort();
         let mut want = want;
         want.sort();
@@ -33,60 +76,3 @@ mod tests {
         assert_eq!(got, want);
     }
 }
-
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-
-//     struct tcase {
-//         name: String,
-//         nums: Vec<i32>,
-//         want: Vec<Vec<i32>>,
-//     }
-
-//     fn do_vecs_match<T: PartialEq>(a: &Vec<T>, b: &Vec<T>) -> bool {
-//         println!("{} {}", a.len(), b.len());
-//         if a.len() != b.len() {
-//             return false;
-//         }
-//         let matching = a.iter().zip(b.iter()).filter(|&(a, b)| a == b).count();
-//         matching == a.len() && matching == b.len()
-//     }
-
-//     #[test]
-//     fn test_subsets() {
-//         let tcs = vec![tcase {
-//             name: "example1".to_string(),
-//             nums: vec![1, 2, 3],
-//             want: vec![
-//                 vec![],
-//                 vec![1],
-//                 vec![2],
-//                 vec![1, 2],
-//                 vec![3],
-//                 vec![1, 3],
-//                 vec![2, 3],
-//                 vec![1, 2, 3],
-//             ],
-//         }];
-//         for mut tc in tcs {
-//             // sort
-//             let mut got = Solution::subsets(tc.nums);
-//             got.sort();
-//             tc.want.sort();
-//             let want = tc.want;
-
-//             println!("got: {:?}", got);
-//             println!("want: {:?}", want);
-
-//             // assert_eq!(got.len(), want.len());
-//             assert_eq!(do_vecs_match(&got, &want), true);
-
-//             // for (outer_got, outer_want) in got.iter().zip(want.iter()) {
-//             //     for (inner_got, inner_want) in outer_got.iter().zip(outer_want.iter()) {
-//             //         assert_eq!(inner_got, inner_want);
-//             //     }
-//             // }
-//         }
-//     }
-// }
