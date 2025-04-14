a906bf647551af5555136f439bbcf2803475ff63
Eric Chang <e850506@gmail.com>
2025-04-15 07:55:08 +0800

    wip: binary_tree: add

--- leetcode/rs/src/binary_tree.rs
+++ leetcode/rs/src/binary_tree.rs
@@ -5,6 +5,21 @@ struct BinaryTree<T> {
     pub root: NodeType<T>,
 }
 
+impl<T> Into<Vec<T>> for BinaryTree<T> {
+    fn into(self) -> Vec<T> {
+        vec![]
+    }
+}
+
+impl<T> From<Vec<T>> for BinaryTree<T> {
+    fn from(eles: Vec<T>) -> Self {
+        BinaryTree::new(Some(Rc::new(RefCell::new(TreeNode {
+            left: None,
+            right: None,
+        }))))
+    }
+}
+
 type NodeType<T> = Option<Rc<RefCell<TreeNode<T>>>>;
 
 #[derive(Debug, PartialEq)]
@@ -48,5 +63,14 @@ mod tests {
         })));
         let t: BinaryTree<i32> = BinaryTree::new(root.clone());
         assert_eq!(t.root, root);
+    }
+
+    #[rstest]
+    #[case::empty(vec![], vec![])]
+    #[case::only_one_element(vec![1], vec![1])]
+    fn test_from(#[case] eles: Vec<i32>, #[case] want_eles: Vec<i32>) {
+        let t: BinaryTree<i32> = BinaryTree::from(eles);
+        let a: Vec<i32> = t.into();
+        assert_eq!(a, want_eles);
     }
 }
