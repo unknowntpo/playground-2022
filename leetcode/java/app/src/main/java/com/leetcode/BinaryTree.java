68a6e125b4583174e35511cfdf70c7292ff16ded
Eric Chang <e850506@gmail.com>
2025-04-22 08:36:29 +0800

    feat(leetcode/java): BinaryTree: todo nullable

--- leetcode/java/app/src/main/java/com/leetcode/BinaryTree.java
+++ leetcode/java/app/src/main/java/com/leetcode/BinaryTree.java
@@ -1,7 +1,40 @@
 package com.leetcode;
 
+import javax.annotation.Nullable;
+import javax.swing.text.html.Option;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 public class BinaryTree {
+    public static BinaryTree of(List<Integer> nums) {
+        return new BinaryTree();
+    }
+
+    public static List<Integer> into(BinaryTree tree) {
+        return new ArrayList<>();
+    }
+
     public int add(int a, int b) {
         return a + b;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode() {
+        }
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+
+        public TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
     }
 }
--- leetcode/java/app/src/test/java/com/leetcode/BinaryTreeTest.java
+++ leetcode/java/app/src/test/java/com/leetcode/BinaryTreeTest.java
@@ -6,38 +6,45 @@ import org.junit.runners.Parameterized;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 @RunWith(Parameterized.class)
 public class BinaryTreeTest {
     private final String name;
-    private final int want;
-    private final int a;
-    private final int b;
+    private final int[] want;
+    private final int[] nums;
 
-    public BinaryTreeTest(String name, int a, int b, int want) {
+    public BinaryTreeTest(String name, int[] nums, int[] want) {
         this.name = name;
+        this.nums = nums;
         this.want = want;
-        this.a = a;
-        this.b = b;
     }
 
     @Parameterized.Parameters(name = "{0}")
     public static Collection<Object[]> testData() {
         return Arrays.asList(
                 new Object[][]{
-                        {"case0", 1, 3, 6},
-                        {"case1", 1, 2, 3},
-                        {"case2", 1, 2, 3}
+                        {"empty list", new int[]{}, new int[]{}},
+                        {"list with 1 element", new int[]{1}, new int[]{1}},
+                        {"list with multiple elements", new int[]{1, 2, 3}, new int[]{1, 2, 3}},
                 }
         );
     }
 
+    //    @Test
+//    public void testAdd() {
+//        BinaryTree binaryTree = new BinaryTree();
+//        assertEquals(want, binaryTree.add(a, b));
+//    }
+//
     @Test
-    public void testAdd() {
-        BinaryTree binaryTree = new BinaryTree();
-        assertEquals(want, binaryTree.add(a, b));
+    public void testBuildTree() {
+        List<Integer> got = BinaryTree.into(BinaryTree.of(List.of(3)));
+
+        assertEquals(want, got.toArray(Integer[]::new));
     }
 }
