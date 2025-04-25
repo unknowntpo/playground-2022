ad8708b40759680bda038e9f8c5b3d5ba3b4aba0
Eric Chang <e850506@gmail.com>
2025-04-25 14:42:04 +0800

    refactor(python/fastapi_example): add type hint

--- python/fastapi_example/main.py
+++ python/fastapi_example/main.py
@@ -6,9 +6,9 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
-async def read_root():
+async def read_root() -> dict:
     return {"Hello": "World"}
 
 @app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Union[str, None] = None):
+async def read_item(item_id: int, q: Union[str, None] = None) -> dict:
     return {"item_id": item_id, "q": q}
