af1a8c9015d6231d76f15715c96e92c84fe05fc4
Eric Chang <e850506@gmail.com>
2025-05-06 09:05:13 +0800

    feat(ts/react/react_practice): display keystroke

--- ts/react/react_practice/src/pages/Calculator.tsx
+++ ts/react/react_practice/src/pages/Calculator.tsx
@@ -1,16 +1,35 @@
 // import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import './Calculator.css'
-import CalculatorButton from './CalculatorButton';
+import CalculatorButton, { CalculatorButtonProps } from './CalculatorButton';
 
 export default function Calculator() {
-	const arr: string[] = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "+", "-", "x", "%"];
+	const initExpression = '0';
+	 
+	const [expression, setExpression] = useState(initExpression);
+	const rows: string[][] = [
+		["AC", "+/-", "%", "÷"],
+		["7", "8", "9", "x"],
+		["4", "5", "6", "-"],
+		["1", "2", "3", "+"],
+		["", "0", ".", "="]
+	];
+
+	const handleClick = (char: CalculatorButtonProps["value"]) => {
+		console.log(`${char} is clicked`)
+		setExpression(prevExpression => prevExpression === initExpression ? char : prevExpression + char);
+	}
 
 	return (
-		<div className="flex flex-row">
-			{arr.map((char) => (
-					<CalculatorButton value={char} />
-			))}
-
-		</div>
+		<div className="flex flex-col gap-2"> {/* Changed to column layout with gap */}
+		<div className="bg-green-50">{expression}</div>
+		{rows.map((row, rowIndex) => (
+				<div key={rowIndex} className="flex flex-row gap-2"> {/* Row container with gap */}
+						{row.map((char) => (
+								<CalculatorButton key={char} value={char} onClick={handleClick}/>
+						))}
+				</div>
+		))}
+	</div>
 	)
 } 
--- ts/react/react_practice/src/pages/CalculatorButton.tsx
+++ ts/react/react_practice/src/pages/CalculatorButton.tsx
@@ -1,14 +1,17 @@
 // import { useEffect, useState } from 'react';
 import './CalculatorButton.css'
 
-interface CalculatorButtonProps 
+export interface CalculatorButtonProps 
 {
 	value: string;
+	onClick: (value: CalculatorButtonProps["value"]) => void;
 }
 
-export default function CalculatorButton({value}: CalculatorButtonProps) {
+export default function CalculatorButton({value, onClick}: CalculatorButtonProps) {
 	return (
-		<div>
+		<div className="w-12 h-12 flex items-center justify-center bg-gray-200 rounded-lg cursor-pointer 
+	  hover:bg-grey-300
+		" onClick={() => onClick(value)}>
 			{value}
 		</div>
 	)
