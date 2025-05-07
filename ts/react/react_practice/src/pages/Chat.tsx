e137915f1c8b87d0a8a9e967b1f04028cf75468a
Eric Chang <e850506@gmail.com>
2025-05-08 07:53:11 +0800

    refactor(ts/react/react_practice/Chat): add Broadcast channel hook

--- ts/react/react_practice/src/pages/Chat.tsx
+++ ts/react/react_practice/src/pages/Chat.tsx
@@ -1,18 +1,26 @@
 import { useState } from 'react';
+import { useBroadcastChannel } from '../hooks/BroadcastChannel';
 
 export default function Chat() {
 		const [messages, setMessages] = useState<string[]>([]); 
 		const [inputValue, setInputValue] = useState<string>(''); 
+		const testName = "Hello";
+		const {message: gotMessage, sendMessage} = useBroadcastChannel(testName); 
 
 		const handleSubmit = (e: React.FormEvent) => {
 			e.preventDefault();
 			messages.push(inputValue);
 			setMessages(messages);
+			sendMessage(inputValue);
 			setInputValue('');
 		};
     return (
         <div className="chat-container">
 					<div className="messages">
+						{/* FIXME: try to merge messages and gotMessage */}
+						{gotMessage && (
+							<div className="got-message">{gotMessage}</div>
+						)}
 						{messages.map((msg, index) => (
 							<div key={index} className="message">{msg}</div>
 						))}
