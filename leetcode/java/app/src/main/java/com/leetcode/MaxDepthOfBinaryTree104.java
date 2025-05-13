74996be78aedf3730f2a3239a2fe38bd20a4410f
Eric Chang <e850506@gmail.com>
2025-05-13 08:49:39 +0800

    feat(leetcode/java/MaxDepthOfBinaryTree104): PASS

---
+++ leetcode/java/app/src/main/java/com/leetcode/MaxDepthOfBinaryTree104.java
@@ -0,0 +1,44 @@
+package com.leetcode;
+
+import com.leetcode.BinaryTree.TreeNode;
+
+
+/**
+ * 给定一个二叉树 root ，返回其最大深度。
+ * <p>
+ * 二叉树的 最大深度 是指从根节点到最远叶子节点的最长路径上的节点数。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：root = [3,9,20,null,null,15,7]
+ * 输出：3
+ * 示例 2：
+ * <p>
+ * 输入：root = [1,null,2]
+ * 输出：2
+ */
+public class MaxDepthOfBinaryTree104 {
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode() {}
+     * TreeNode(int val) { this.val = val; }
+     * TreeNode(int val, TreeNode left, TreeNode right) {
+     * this.val = val;
+     * this.left = left;
+     * this.right = right;
+     * }
+     * }
+     */
+    static class Solution {
+        public int maxDepth(TreeNode root) {
+            if (root == null) {
+                return 0;
+            }
+            return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+        }
+    }
+}
---
+++ leetcode/java/app/src/test/java/com/leetcode/MaxDepthOfBinaryTree104Test.java
@@ -0,0 +1,44 @@
+package com.leetcode;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RunWith(Parameterized.class)
+public class MaxDepthOfBinaryTree104Test extends TestCase {
+    private final String name;
+    private final List<Integer> input;
+    private final int want;
+
+    public MaxDepthOfBinaryTree104Test(String name, List<Integer> input, int want) {
+        this.name = name;
+        this.input = input;
+        this.want = want;
+    }
+
+    @Parameterized.Parameters(name = "{0}")
+    public static List<Object[]> testData() {
+        return List.of(
+                new Object[]{"empty", List.of(), 0},
+                new Object[]{"one", List.of(1), 1},
+                new Object[]{"two", Arrays.asList(1, 2), 2},
+                new Object[]{"three", Arrays.asList(1, 2, 3), 2},
+                new Object[]{"five", Arrays.asList(1, 2, null, 3, 4), 3},
+                new Object[]{"official example 1", Arrays.asList(3, 9, 20, null, null, 15, 7), 3},
+                new Object[]{"official example 2", Arrays.asList(1, null, 2), 2}
+        );
+    }
+
+    @Test
+    public void test() {
+        MaxDepthOfBinaryTree104.Solution solution = new MaxDepthOfBinaryTree104.Solution();
+        BinaryTree tree = BinaryTree.of(input);
+        int got = solution.maxDepth(tree.getRoot());
+        assertEquals(want, got);
+    }
+}
+
