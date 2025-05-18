fcbf3b59379a600ece6f389e28b70a707a15bb15
Eric Chang <e850506@gmail.com>
2025-05-18 18:19:02 +0800

    feat(python/fastapi_example): ChatMessage: insert to db

--- python/fastapi_example/app/__tests__/routers/v1/test_ChatRouter.py
+++ python/fastapi_example/app/__tests__/routers/v1/test_ChatRouter.py
@@ -3,18 +3,41 @@ import json
 
 import pytest
 from fastapi.testclient import TestClient
+from sqlmodel import Session, select
 
+from app.__tests__.utils.utils import trunk_all_tables
+from app.entities.chat_message import ChatMessage
 from app.entities.item import Item
+from app.infra.engine import engine
 from app.main import app
+from app.routers.v1.ChatRouter import ChatMessageCreateRequest
+from app.routers.v1.HeroRouter import get_session
+
 
 # Create a fixture that will be used for all tests
+# Use `with` to use TestClient as a context manager
 @pytest.fixture
 def client():
-    return TestClient(app)
+    with TestClient(app) as client:
+        yield client
+
+
+@pytest.fixture(autouse=True)
+def before_each():
+    trunk_all_tables()
+
 
 def test_ChatRouter_chat(client):
     with client.websocket_connect("/v1/chat/ws") as ws:
-        data = "hello"
-        ws.send_text(data)
-        got = ws.receive_text()
-        assert got == f"Message text was: {data}"
+        msg = ChatMessageCreateRequest(content="Hello from user1")
+        ws.send_json(msg.model_dump())
+        got = ws.receive_json()
+        with Session(engine) as session:
+            rows = session.exec(select(ChatMessage)).all()
+            assert len(rows) == 1
+            # FIXME: why this is ChatMessage, not sqlalchemy.engine.row.Row ?
+            ChatMessage
+            got_msg = rows[0]
+            assert got_msg.content == msg.content
+        # When userA connected, it get all history messages, and when it send data,
+        # userB will get all messages.
--- python/fastapi_example/app/routers/v1/ChatRouter.py
+++ python/fastapi_example/app/routers/v1/ChatRouter.py
@@ -1,24 +1,26 @@
-from typing import Union
+from typing import Union, Annotated
 
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, Depends
 from pydantic import BaseModel
+from sqlmodel import Session
 from starlette.responses import HTMLResponse
 from starlette.websockets import WebSocket, WebSocketDisconnect
 
+from app.entities.chat_message import ChatMessageBase, ChatMessage
 from app.entities.item import Item
+from app.infra.engine import engine
 
 
-class ItemResponse(BaseModel):
-    item_id: int
-    q: Union[str, None] = None
-
-
-class CreateItemResponse(BaseModel):
-    item_name: str
-    item_id: int
-
+class ChatMessageCreateRequest(ChatMessageBase):
+    pass
 
 ChatRouter: APIRouter = APIRouter(prefix="/v1/chat", tags=["chat"])
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+SessionDep = Annotated[Session, Depends(get_session)]
 
 """
 Reference:
@@ -66,12 +68,16 @@ async def get():
 
 
 @ChatRouter.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
+async def websocket_endpoint(websocket: WebSocket, session: SessionDep):
     await websocket.accept()
     try:
         while True:
-            data = await websocket.receive_text()
-            await websocket.send_text(f"Message text was: {data}")
+            msg_json = await websocket.receive_json()
+            msg = ChatMessage(**msg_json)
+            session.add(msg)
+            session.commit()
+            session.refresh(msg)
+            await websocket.send_json(msg.model_dump())
     except WebSocketDisconnect:
         print("Disconnected")
 
