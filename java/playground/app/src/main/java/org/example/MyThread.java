5815b65bc555dc681c298f31d558207b00e3fa38
Eric Chang <e850506@gmail.com>
2025-07-29 10:24:03 +0800

    feat(java/playground): MyThread: send hello to queue

--- java/playground/app/src/main/java/org/example/MyThread.java
+++ java/playground/app/src/main/java/org/example/MyThread.java
@@ -1,18 +1,57 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Queue;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class MyThread extends Thread {
-    private List<String> list;
+    private final int repeat;
+    private final Queue<String> queue;
 
-    public MyThread(List<String> list) {
-        this.list = list;
+    public MyThread(Queue<String> queue, int repeat) {
+        this.repeat = repeat;
+        this.queue = queue;
     }
 
     @Override
     public void run() {
-        for (int i = 0; i < 5; i++) {
-            list.add("Hello");
+        var threads = Stream.generate(() -> new HelloThread(queue, 1)).limit(repeat).toList();
+//        var threads = Collections.nCopies(repeat, new HelloThread(queue, 1));
+
+        for (int i = 0; i < repeat; i++) {
+            System.out.printf("starting thread %d\n", i);
+            var t = threads.get(i);
+            t.start();
+        }
+
+        threads.forEach((t) -> {
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
+    public class HelloThread extends Thread {
+        private final Queue<String> queue;
+        private final int repeat;
+
+        public HelloThread(Queue<String> queue, int repeat) {
+            this.queue = queue;
+            this.repeat = repeat;
+        }
+
+        @Override
+        public void run() {
+            System.out.printf("thread %s started\n", String.format("thread-%d", new Random().nextInt()));
+            for (int i = 0; i < repeat; i++) {
+                queue.add("Hello");
+            }
         }
     }
 }
--- java/playground/app/src/test/java/org/example/MyThreadTest.java
+++ java/playground/app/src/test/java/org/example/MyThreadTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedDeque;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -11,21 +14,21 @@ class MyThreadTest {
 
     @Test
     public void testMyThreadRun() throws InterruptedException {
-        var l = new ArrayList<String>();
-        var t = new MyThread(l);
+        Queue<String> q = new ConcurrentLinkedDeque<>();
+        var t = new MyThread(q, 1);
 
         t.start();
         Thread.sleep(100);
-        assertEquals(2, 1+1);
+        assertEquals(2, 1 + 1);
     }
 
     @Test
     public void testPrintHello() throws InterruptedException {
-        var l = new ArrayList<String>();
-        var t = new MyThread(l);
+        Queue<String> queue = new ConcurrentLinkedDeque<>();
+        var t = new MyThread(queue, 5);
 
         t.start();
         Thread.sleep(100);
-        assertEquals(l,Collections.nCopies(5, "Hello"));
+        assertEquals(Collections.nCopies(5, "Hello"), new ArrayList<>(queue));
     }
 }
