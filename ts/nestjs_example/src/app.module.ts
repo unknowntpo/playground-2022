06db86fe01882a84a1e2e45cd67fd36306fee556
Eric Chang <e850506@gmail.com>
2025-09-13 19:49:35 +0800

    refactor(ts/nextjs_example): make pnpm start:dev work

--- ts/nestjs_example/src/app.module.ts
+++ ts/nestjs_example/src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TodosModule } from '@/todos/todos.module';
+import { ConfigModule } from '@nestjs/config';
+import configuration from '@/config/configuration';
 
 @Module({
-  imports: [TodosModule],
+  imports: [TodosModule, ConfigModule.forRoot({ load: [configuration] })],
 })
 export class AppModule {}
