45192c94eca603135caeaa45652f8ee29e48779b
Eric Chang <e850506@gmail.com>
2025-09-17 11:05:44 +0800

    feat(ts/stream-example): make async definitely async with nextTick

--- ts/stream-example/src/nextTick_setImmediate.ts
+++ ts/stream-example/src/nextTick_setImmediate.ts
@@ -36,4 +36,40 @@ server.listen(0, () => {
 
 console.log('=== End of synchronous code ===');
 
+console.log('\n=== API Consistency Example ===');
 
+function badAsyncAPI(useCache: boolean, callback: (data: string) => void) {
+    if (useCache) {
+        // Sync return - inconsistent!
+        console.log('Returning cached data synchronously');
+        callback('cached data');
+    } else {
+        // Async return
+        console.log('Reading file asynchronously');
+        setTimeout(() => callback('file data'), 10);
+    }
+}
+
+function goodAsyncAPI(useCache: boolean, callback: (data: string) => void) {
+    if (useCache) {
+        console.log('Returning cached data with nextTick');
+        process.nextTick(() => callback('cached data'));
+    } else {
+        console.log('Reading file asynchronously');
+        setTimeout(() => callback('file data'), 10);
+    }
+}
+
+setTimeout(() => {
+    console.log('--- Testing bad API ---');
+    console.log('Before calling badAsyncAPI with cache');
+    badAsyncAPI(true, (data) => console.log('BAD API Callback received:', data));
+    console.log('After calling badAsyncAPI with cache');
+}, 100);
+
+setTimeout(() => {
+    console.log('\n--- Testing good API ---');
+    console.log('Before calling goodAsyncAPI with cache');
+    goodAsyncAPI(true, (data) => console.log('GOOD API Callback received:', data));
+    console.log('After calling goodAsyncAPI with cache');
+}, 200);
