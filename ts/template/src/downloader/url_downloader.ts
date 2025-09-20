3186841bdaec2c30267042a1ec7ecdce78afe194
Eric Chang <e850506@gmail.com>
2025-09-20 08:30:34 +0800

    feat(ts/template): add url_downloader

---
+++ ts/template/src/downloader/url_downloader.ts
@@ -0,0 +1,17 @@
+
+interface Task {
+    id: number
+    name: string,
+    url: string,
+}
+
+class Downloader {
+    constructor() {
+    }
+
+    public async addTask() {}
+
+
+}
+
+export {Downloader};
