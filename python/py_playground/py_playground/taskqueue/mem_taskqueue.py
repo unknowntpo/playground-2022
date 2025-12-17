65b61ae69aa68532d31a376c80e80af9b13e260b
Eric Chang <e850506@gmail.com>
2025-12-17 19:42:38 +0800

    feat(python/py_playground): mem task queue: submit multiple tasks

--- python/py_playground/py_playground/taskqueue/mem_taskqueue.py
+++ python/py_playground/py_playground/taskqueue/mem_taskqueue.py
@@ -1,12 +1,11 @@
 import asyncio
-from asyncio import Queue
-from typing import Callable, Coroutine
+import logging
+from typing import Callable, Coroutine, Any
 
 from py_playground.taskqueue import TaskQueue
 from py_playground.taskqueue.taskqueue import Task, Status
 
-
-class MemTask(Task):
+class MemTask:
     def __init__(self, fn: Callable):
         self._fn = fn
         self._status = Status.INITIALIZED
@@ -16,16 +15,21 @@ class MemTask(Task):
         return self._future.__await__()
 
     @property
-    def status(self) -> Status :
+    def status(self) -> Status:
         return self._status
 
     @status.setter
     def status(self, value: Status):
         self._status = value
 
+    async def run(self):
+        res = self._fn()
+        self._future.set_result(res)
+
+
 class MemTaskQueue(TaskQueue):
     def __init__(self):
-        self._queue = asyncio.Queue[Task]()
+        self._queue = asyncio.Queue[MemTask]()
         self._worker_task = None
 
     async def run(self):
@@ -33,22 +37,24 @@ class MemTaskQueue(TaskQueue):
 
     async def submit(self, *, fn: Callable) -> Task:
         t = MemTask(fn)
+        t.status = Status.INITIALIZED
         await self._queue.put(t)
         return t
 
     async def _worker(self):
-        while True:
-            task: Task = await self._queue.get()
-            try:
-                task.status = Status.RUNNING
-                result = await task
-                task.status = Status.SUCCESS
-                task.set_result(result)
-            except Exception as e:
-                task.set_exception(e)
-                task.status = Status.FAILED
-            finally:
-                self._queue.task_done()
+        try:
+            while True:
+                task: MemTask = await self._queue.get()
+                try:
+                    task.status = Status.RUNNING
+                    result = await task.run()
+                    task.status = Status.SUCCESS
+                except Exception as e:
+                    task.set_exception(e)
+                    task.status = Status.FAILED
+        except asyncio.CancelledError:
+            logging.debug("worker got cancelled")
+            pass
 
     async def stop(self):
         self._worker_task.cancel()
--- python/py_playground/tests/unit/taskqueue/test_mem_taskqueue.py
+++ python/py_playground/tests/unit/taskqueue/test_mem_taskqueue.py
@@ -1,16 +1,45 @@
+import asyncio
 from unittest.result import failfast
 
 from py_playground.taskqueue.mem_taskqueue import MemTaskQueue
 from py_playground.taskqueue.taskqueue import Task
 
-async def test_submit() -> None:
+async def test_submit():
     queue = MemTaskQueue()
-    x: int = 3
+    x = 3
     await queue.run()
 
-    try:
-        task: Task = await queue.submit(fn=lambda: x + 1)
-        await task
-        await queue.stop()
-    finally:
-        assert 4 == x
+    def add_x():
+        nonlocal x
+        x += 1
+
+    task: Task = await queue.submit(fn=add_x)
+    await task
+    await queue.stop()
+    assert x == 4
+
+async def test_submit_multiple():
+    queue = MemTaskQueue()
+    await queue.run()
+
+    x = 3
+    def add_x():
+        nonlocal x
+        x += 1
+
+    content = ""
+    def add_content():
+        nonlocal content
+        content += "hello"
+
+    tasks = [
+        queue.submit(fn=add_x),
+        queue.submit(fn=add_content),
+        queue.submit(fn=add_content)
+    ]
+
+    await asyncio.gather(*tasks)
+
+    await queue.stop()
+    assert x == 4
+    assert content == "hellohello"
