c7b62e18f01b3ba5c794f9fa08fd65b0ba9d81ce
Eric Chang <e850506@gmail.com>
2025-12-23 11:03:14 +0800

    feat(python/fastapi_example): init

--- python/fastapi_example/app/routers/v1/healthz.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-
-HealthzRouter: APIRouter = APIRouter(prefix="/v1/healthz", tags=["healthz"])
-
-@HealthzRouter.get("")
-async def healthcheck() -> dict:
-    return {"status": "ok"}
