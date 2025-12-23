294792d9ff4aa7192aa03f3c1511d2456035a23d
Eric Chang <e850506@gmail.com>
2025-12-23 20:59:10 +0800

    feat(python/fastapi_example): allow multiple symbols

--- python/fastapi_example/tests/routers/test_stock.py
+++ python/fastapi_example/tests/routers/test_stock.py
@@ -7,9 +7,6 @@ client = TestClient(app)
 
 
 def test_get_stock_pricing():
-    resp = client.get("/stock/pricing", params={"symbols": ["AAPL"]})
+    resp = client.get("/stock/pricing", params={"symbols": ["TSLA", "AAPL"]})
     assert http.client.OK == resp.status_code
-    assert {"symbols": {"AAPL": 3310.0}} == resp.json()
-    resp = client.get("/stock/pricing", params={"symbols": ["TSLA"]})
-    assert http.client.OK == resp.status_code
-    assert {"symbols": {"TSLA": 200.4}} == resp.json()
+    assert {"symbols": {"TSLA": 200.4, "AAPL": 3310.0}} == resp.json()
