eb7a12ff4f491b7e238ac1c90cc869da5e3ca841
Eric Chang <e850506@gmail.com>
2026-01-01 07:14:54 +0800

    feat(python/py_playground/arithmetic_parser): impl single number parsing

--- python/py_playground/py_playground/arithmetic_parser/parser.py
+++ python/py_playground/py_playground/arithmetic_parser/parser.py
@@ -5,17 +5,23 @@ from enum import Enum
 class Type(Enum):
     Number = "number"
     Plus = "+"
+    Minus = "-"
 
 
 class Token:
     def __init__(self, type: Type, value: str):
-        self._type = type
-        self._value = value
+        self.type = type
+        self.value = value
 
 """
 
 
 """
+
+
+class ParseError(Exception):
+    pass
+
 class Parser:
     """
     arithemetic parser
@@ -23,8 +29,7 @@ class Parser:
     term   → factor ( ( "*" | "/" ) factor )*
     factor → unary | NUMBER | "(" expr ")"
     unary  → ( "-" | "+" )? factor
-    NUMBER → digit+ ( "." digit+ )?
-    digit  → "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9"
+    NUMBER → <any valid number string from lexer>
     """
 
     def __init__(self):
@@ -33,16 +38,29 @@ class Parser:
 
     def current(self) -> Token | None:
         return self._tokens[self.pos] if len(self._tokens) > 0 and self.pos < len(self._tokens) else None
+
     def parse(self, tokens: list[Token])-> Decimal:
         self._tokens = tokens
         return self.term()
 
     def term(self) -> Decimal:
-        return self.factor()
+        res = self.factor()
+        if (current := self.current()) and current is not None and current.type in (Type.Plus, Type.Minus):
+            rhr = self.term()
+            res += rhr
+        return res
 
     def factor(self) -> Decimal:
+        if self.current().type == Type.Number:
+            return self.number()
+
         return self.unary()
 
-
+    def number(self) -> Decimal:
+        if self.current() is None:
+            raise ParseError(f"current token should not be None, pos: {self.pos}")
+        d = Decimal(self.current().value)
+        self.pos += 1
+        return d
 
 
--- python/py_playground/tests/unit/arithmetic_parser/test_parser.py
+++ python/py_playground/tests/unit/arithmetic_parser/test_parser.py
@@ -3,9 +3,12 @@ from decimal import Decimal
 from py_playground.arithmetic_parser.parser import Parser, Token, Type
 
 
-def test_add():
-    # (1 + 2) / 3
-    # 1 + 1
+def test_single_number():
     p = Parser()
-    res: Decimal = p.parse([Token(type=Type.Number, value="2"), Token(type=Type.Number, value="1")])
-    assert res == 3
+    res: Decimal = p.parse([Token(type=Type.Number, value="2")])
+    assert res == 2
+
+def test_add_two_numbers():
+    p = Parser()
+    res: Decimal = p.parse([Token(type=Type.Number, value="2"), Token(type=Type.Plus, value="+"), Token(type=Type.Number, value="3")])
+    assert res == 5
