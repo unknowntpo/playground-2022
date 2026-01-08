c60de8ef1e77df358b8af723d3a27ffca161cb34
Eric Chang <e850506@gmail.com>
2026-01-08 21:06:47 +0800

    fix(java/playground/concurrency/PrintInOrder): use CountDownLatch

--- java/playground/app/src/test/java/org/example/concurrency/PrintInOrder.java
+++ java/playground/app/src/test/java/org/example/concurrency/PrintInOrder.java
@@ -1,60 +1,33 @@
 package org.example.concurrency;
 
+import java.util.concurrent.CountDownLatch;
+
 /**
  * Ref: https://leetcode.cn/problems/print-in-order/description/
  */
 class PrintInOrder {
 
-    private Integer value = 0;
+    private CountDownLatch latch0 = new CountDownLatch(1);
+    private CountDownLatch latch1 = new CountDownLatch(1);
 
     public PrintInOrder() {
 
     }
 
     public void first(Runnable printFirst) throws InterruptedException {
-        while (true) {
-            synchronized (this.value) {
-                if (value == 0) {
-                    printFirst.run();
-                    value += 1;
-                    break;
-                }
-            }
-            Thread.sleep(1);
-        }
-        // printFirst.run() outputs "first". Do not change or remove this line.
-//        printFirst.run();
+        printFirst.run();
+        latch0.countDown();
     }
 
     public void second(Runnable printSecond) throws InterruptedException {
-        while (true) {
-            synchronized (this.value) {
-                if (value == 1) {
-                    printSecond.run();
-                    value += 1;
-                    break;
-                }
-            }
-            Thread.sleep(1);
-        }
-        // printSecond.run() outputs "second". Do not change or remove this line.
-//        printSecond.run();
+        latch0.await();
+        printSecond.run();
+        latch1.countDown();
     }
 
     public void third(Runnable printThird) throws InterruptedException {
-        while (true) {
-            synchronized (this.value) {
-                if (value == 2) {
-                    printThird.run();
-                    value += 1;
-                    break;
-                }
-            }
-            Thread.sleep(1);
-        }
-
-        // printThird.run() outputs "third". Do not change or remove this line.
-//        printThird.run();
+        latch1.await();
+        printThird.run();
     }
 }
 
