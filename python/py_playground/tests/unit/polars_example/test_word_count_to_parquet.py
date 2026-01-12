82868f74dd01f8217418e3c68e1d01c4196c9921
Eric Chang <e850506@gmail.com>
2026-01-12 10:46:16 +0800

    feat(python/py_playground/polars_example): play with parquet

--- python/py_playground/tests/unit/polars_example/test_word_count_to_parquet.py
+++ python/py_playground/tests/unit/polars_example/test_word_count_to_parquet.py
@@ -1,3 +1,6 @@
+import os.path
+import pathlib
+
 import polars as pl
 
 def test_polars():
@@ -7,3 +10,26 @@ def test_polars():
     })
     res = df.select("col1").filter(pl.col("col1") % 2 == 1).sum()
     assert res.item() == 4
+
+def test_polars_parquet():
+    df = pl.DataFrame({
+        "col1": [1, 3, 4],
+        "col2": [5,6,7],
+    })
+
+    path = "/tmp/test0031.parquet"
+
+    filepath = pathlib.Path(path)
+
+    if os.path.exists(filepath):
+        os.remove(filepath)
+
+    df.write_parquet(path)
+
+    assert filepath.is_file()
+
+    df2 = pl.read_parquet(filepath)
+    print(df2)
+
+    assert 1 == 1
+
