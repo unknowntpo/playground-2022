64a4f1b309c55745f3e631b4a07ff7a1a8d5a47d
Eric Chang <e850506@gmail.com>
2026-01-13 10:40:14 +0800

    refactor(python/airflow_example):  extract flush_to_parquet for write_parquet_from_generator

--- python/airflow_example/dags/services/game_datasource.py
+++ python/airflow_example/dags/services/game_datasource.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 import polars as pl
 from pathlib import Path
-from typing import Protocol, Iterator, Self
+from typing import Protocol, Iterator, Self, Any
 
 from dags.services.game_event import EventType, Event
 from dags.services.gamedata_generator import GameDataGenerator
@@ -46,23 +46,20 @@ def write_parquet_from_generator(gen: GameDataGenerator, output_file: Path):
     for event in gen:
         print(f"got event: {event}")
         if len(batch) >= max_batch_size:
-            df = pl.DataFrame(batch)
-            if Path(output_file).exists():
-                existing = pl.read_parquet(output_file)
-                df = pl.concat([existing, df])
-                df.write_parquet(output_file)
-            else:
-                df.write_parquet(output_file)
+            flush_to_parquet(batch, output_file)
             batch = []
         batch.append(event.model_dump(mode="json"))
 
     if batch:
-        df = pl.DataFrame(batch)
-        if Path(output_file).exists():
-            existing = pl.read_parquet(output_file)
-            df = pl.concat([existing, df])
-            df.write_parquet(output_file)
-        else:
-            df.write_parquet(output_file)
+        flush_to_parquet(batch, output_file)
+
+def flush_to_parquet(batch: list[Any], output_file: Path):
+    df = pl.DataFrame(batch)
+    if Path(output_file).exists():
+        existing = pl.read_parquet(output_file)
+        df = pl.concat([existing, df])
+        df.write_parquet(output_file)
+    else:
+        df.write_parquet(output_file)
 
 
